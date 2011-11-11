3436d36958e996665c179f9966fba723b20cad50
Ben Hearsum <bhearsum@mozilla.com>
2011-11-11 14:48:45 -0500

    bug 701382: balrog's standalone admin server needs to do authentication. r=catlee

--- admin.py
+++ admin.py
@@ -1,7 +1,13 @@
+from paste.auth.basic import AuthBasicHandler
+
 from auslib.web.base import app, db
 
 db.setDburi('sqlite:////tmp/bhearsum.db')
 db.createTables()
 app.config['SECRET_KEY'] = 'abc123'
 app.config['DEBUG'] = True
+
+def auth(environ, username, password):
+    return username == password
+app.wsgi_app = AuthBasicHandler(app.wsgi_app, "Balrog standalone auth", auth)
 app.run()
