b1f959b825544d6cc6f8a61129c17ed0b0e186f2
Ben Hearsum <bhearsum@mozilla.com>
2011-12-20 16:45:54 -0500

    bug 712430: add extra logging for HTTP 500 errors in balrog. r=catlee

--- auslib/web/base.py
+++ auslib/web/base.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
+from auslib import version
 from auslib.db import AUSDatabase
 
 app = Flask(__name__)
 db = AUSDatabase()
+
+@app.errorhandler(500)
+def isa(error):
+    log.error("Caught ISA 500 error.")
+    log.debug("Balrog version is: %s", version)
+    log.debug("Request path is: %s", request.path)
+    log.debug("Request environment is: %s", request.environ)
+    log.debug("Request headers are: %s", request.headers)
+    return error
 
 # All of our View modules contain routing information that needs to be imported
 # to be active.
