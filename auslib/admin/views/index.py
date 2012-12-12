e8f09aa6c1e06837d5ac8a07fea9c1dceef49c2a
Peter Bengtsson <mail@peterbe.com>
2012-12-11 16:24:53 -0800

    more progress

--- auslib/admin/views/index.py
+++ auslib/admin/views/index.py
@@ -33,23 +33,29 @@ class IndexPageView(AdminView):
                 ))
         # youngest first
         all.sort(lambda x, y: cmp(y[1]['timestamp'], x[1]['timestamp']))
-        #all = all[:limit]
+        # the list is cut short (to the length of `limit`) by a break in the loop
         changes = collections.defaultdict(dict)
         other_values = collections.defaultdict(dict)
         recent_changes = []
         needs_diff = collections.defaultdict(list)
+        needs_previous = collections.defaultdict(list)
         inserts = collections.defaultdict(dict)
         _history_keys = ('change_id', 'changed_by', 'timestamp', 'data_version')
         for label, each in all:
             primary_keys = tables[label].history.base_primary_key
             values = [each[x] for x in primary_keys]
             other_values[label][each['change_id']] = dict(
-              (x, each[x])
-              for x in each
-              if x not in primary_keys
+                (x, each[x])
+                for x in each
+                if x not in primary_keys
             )
-
             changes[label][each['change_id']] = values
+
+            if len(recent_changes) >= limit:
+                # make sure this happens *after* `changes` is populated with
+                # the "next" change
+                break
+
             change = None
             if each['data_version'] > 1:
                 # easy, it's an update
@@ -69,20 +75,17 @@ class IndexPageView(AdminView):
                 # then this was a deletion
                 try:
                     previous_values = changes[label][each['change_id'] + 1]
-                    #print "COMPARE", each['change_id']
-                    #print "\t", previous_values
-                    #print "\t", values
                     if previous_values == values:
                         # it was one of those initial creation rows
                         continue
                     else:
                         change = 'delete'
-                        #print each
+                        needs_previous[label].append(each['change_id'])
 
                 except KeyError:
+                    # happens if this is the very latest change
                     change = 'delete'
-#                print "WHAT ABOUT"
-#                print each
+                    needs_previous[label].append(each['change_id'])
 
             each['change'] = change
 
@@ -98,43 +101,72 @@ class IndexPageView(AdminView):
                 each['time_ago'] = 'seconds ago'
 
             recent_changes.append((label, each))
-            if len(recent_changes) >= limit:
-                break
-
-
-        #print needs_diff
         diffs = collections.defaultdict(dict)
         for label, change_ids in needs_diff.items():
             for change_id in change_ids:
-                #print "\t", changes[label][change_id]
-                #print "\t", changes[label][change_id - 1]
-                #print
-                #print "\t", other_values[label][change_id]
-                #print "\t", other_values[label][change_id - 1]
-                #print
+                try:
+                    # being able to look at the "next" change from the loop
+                    # above is an early optimization.
+                    # If "this" is the last element, then (last + 1) won't
+                    # exist in `other_values` so...
+                    prev_other_values = other_values[label][change_id - 1]
+                except KeyError:
+                    # ...we need to do another lookup just to fetch this data.
+                    table = tables[label]
+                    primary_key_values = changes[label][change_id]
+                    prev_change = table.history.getPrevChange(
+                        change_id,
+                        changes[label][change_id]
+                    )
+                    prev_other_values = dict(
+                        (x, prev_change[x])
+                        for x in prev_change
+                        if x not in primary_keys
+                    )
                 diffs[label][change_id] = PrinterFriendlyDict(
-                    (k, (other_values[label][change_id - 1][k], value))
+                    (k, (prev_other_values[k], value))
                     for k, value in other_values[label][change_id].items()
                     if k not in _history_keys
                 )
-#        print inserts
-        #print diffs
-        data['inserts'] = PrinterFriendlyDict(inserts)
-        #data['inserts'] ={}
-        data['diffs'] = PrinterFriendlyDict(diffs)
-#        print repr(data['diffs'])
-#        print type(data['diffs'])
-#        print data['diffs'].items()
+
+        deletes = collections.defaultdict(dict)
+        for label, change_ids in needs_previous.items():
+            for change_id in change_ids:
+                table = tables[label]
+                primary_keys = table.history.base_primary_key
+                try:
+                    prev_other_values = other_values[label][change_id - 1]
+                except KeyError:
+                    # See comment above on the needs_diff.items() loop
+                    primary_key_values = changes[label][change_id]
+                    prev_change = table.history.getPrevChange(
+                        change_id,
+                        changes[label][change_id]
+                    )
+                    prev_other_values = dict(
+                        (x, prev_change[x])
+                        for x in prev_change
+                        if x not in primary_keys
+                    )
+                deletes[label][change_id] = PrinterFriendlyDict(
+                    (k, changes[label][change_id][i])
+                    for i, k in enumerate(primary_keys)
+                )
+                deletes[label][change_id].update(PrinterFriendlyDict(
+                    (k, prev_other_values[k])
+                    for k in prev_other_values
+                    if k not in _history_keys
+                ))
+
+        data['inserts'] = inserts
+        data['diffs'] = diffs
+        data['deletes'] = deletes
         data['recent_changes'] = recent_changes
 
         return render_template('index.html', **data)
 
 
 class PrinterFriendlyDict(dict):
-
-    #def __init__(self, *args, **kwargs):
-    #    super(PrinterFriendlyDict, self).__init__(*args, **kwargs)
-
 
     def keys(self):
         keys = super(PrinterFriendlyDict, self).keys()
