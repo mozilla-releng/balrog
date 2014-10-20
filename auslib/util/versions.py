3f8776be8ca4e372075ed89be8e872d3569e6682
Nick Thomas <nrthomas@gmail.com>
2014-10-21 12:04:33 +1300

    Bug 1069512, deal with tracebacks for old style w.x.y.z versions, r=bhearsum

--- auslib/util/versions.py
+++ auslib/util/versions.py
@@ -1,9 +1,32 @@
 from distutils.version import StrictVersion
 import re
 
-class MozillaVersion(StrictVersion):
+
+class ModernMozillaVersion(StrictVersion):
     """A version class that is slightly less restrictive than StrictVersion.
        Instead of just allowing "a" or "b" as prerelease tags, it allows any
        alpha. This allows us to support the once-shipped "3.6.3plugin1" and
        similar versions."""
-    version_re = re.compile(r'^(\d+) \. (\d+) (\. (\d+))? ([a-zA-Z]+(\d+))?$', re.VERBOSE)
+    version_re = re.compile(r"""^(\d+) \. (\d+) (\. (\d+))?
+                                ([a-zA-Z]+(\d+))?$""", re.VERBOSE)
+
+
+class AncientMozillaVersion(StrictVersion):
+    """A version class that is slightly less restrictive than StrictVersion.
+       Instead of just allowing "a" or "b" as prerelease tags, it allows any
+       alpha. This allows us to support the once-shipped "3.6.3plugin1" and
+       similar versions.
+       It also supports versions w.x.y.z by transmuting to w.x.z, which
+       is useful for versions like 1.5.0.x and 2.0.0.y"""
+    version_re = re.compile(r"""^(\d+) \. (\d+) \. \d (\. (\d+))
+                                ([a-zA-Z]+(\d+))?$""", re.VERBOSE)
+
+
+def MozillaVersion(version):
+    try:
+        return ModernMozillaVersion(version)
+    except ValueError:
+        if version.count('.') == 3:
+            return AncientMozillaVersion(version)
+        else:
+            raise
