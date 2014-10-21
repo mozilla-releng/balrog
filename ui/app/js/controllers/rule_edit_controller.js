fcc692064aec1a446882ddcb09bc495194072616
Peter Bengtsson <mail@peterbe.com>
2014-10-21 16:11:09 -0700

    [balrog-ui] progress and fixes

--- ui/app/js/controllers/rule_edit_controller.js
+++ ui/app/js/controllers/rule_edit_controller.js
@@ -18,13 +18,19 @@ function ($scope, $modalInstance, CSRFService, RulesService, ReleasesService, ru
     .then(function(csrf_token) {
       RulesService.updateRule($scope.rule.id, $scope.rule, csrf_token)
       .success(function(response) {
-        // console.log('RESPONSE', response);
         $scope.rule.data_version = response.new_data_version;
         angular.copy($scope.rule, $scope.original_rule);
         $scope.saving = false;
         $modalInstance.close();
-      }).error(function() {
-        console.error(arguments);
+      }).error(function(response) {
+        if (typeof response === 'object') {
+          $scope.errors = response;
+        }
+        sweetAlert(
+          "Form submission error",
+          "See fields highlighted in red.",
+          "error"
+        );
       });
     });
 
