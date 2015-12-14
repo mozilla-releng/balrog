33ab2dd5d4a50229c6271942fa9f200a7086ca28
Ben Hearsum <bhearsum@mozilla.com>
2015-12-14 10:40:29 -0500

    Switch to being installable + tox.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name="balrog",
+    version="1.0",
+    description="Mozilla's Update Server",
+    author="Ben Hearsum",
+    author_email="ben@hearsum.ca",
+    packages=["auslib"],
+    install_requires=[
+        "flask==0.10.1",
+        "Werkzeug==0.9.6",
+        "wtforms==2.0.1",
+        "flask-wtf==0.10.2",
+        "sqlalchemy-migrate==0.7.2",
+        "tempita==0.5.1",
+        "decorator==3.3.3",
+        "blinker==1.2",
+        "cef==0.5",
+        "flask-compress==1.0.2",
+        "itsdangerous==0.24",
+        "repoze.lru==0.6",
+    ],
+    url="https://github.com/mozilla/balrog",
+)
