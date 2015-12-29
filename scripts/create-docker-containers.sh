020f690fb3b59d7fcf4ba9554d4088b7ffe8eda9
Ben Hearsum <bhearsum@mozilla.com>
2015-12-29 11:18:01 -0500

    Move a bunch of files around for better organization; fix docker image creation commands to work when script is run from directory other than the root of the Balrog repo.

--- scripts/create-docker-containers.sh
+++ scripts/create-docker-containers.sh
@@ -20,9 +20,9 @@ else
     docker start balrog-database
     # Wait for the database to come up
     sleep 30
-    docker run --rm --net host -it bhearsum/balrog:latest python scripts/manage-db.py -d mysql://balrogadmin:balrogadmin@127.0.0.1/balrog create
-    docker run --rm --net host -it bhearsum/balrog:latest sh -c 'exec mysql -h 127.0.0.1 -P 3306 -u balrogadmin --password=balrogadmin balrog < sample-data.sql'
-    docker run --rm --net host -it bhearsum/balrog:latest sh -c 'exec mysql -h 127.0.0.1 -P 3306 -u balrogadmin --password=balrogadmin -e "insert into permissions (username, permission, data_version) values (\"balrogadmin\", \"admin\", 0);" balrog'
+    docker run --rm --net host -it -v $balrog_repo:/app bhearsum/balrog:latest python scripts/manage-db.py -d mysql://balrogadmin:balrogadmin@127.0.0.1/balrog create
+    docker run --rm --net host -it -v $balrog_repo:/app bhearsum/balrog:latest sh -c 'exec mysql -h 127.0.0.1 -P 3306 -u balrogadmin --password=balrogadmin balrog < /app/scripts/sample-data.sql'
+    docker run --rm --net host -it -v $balrog_repo:/app bhearsum/balrog:latest sh -c 'exec mysql -h 127.0.0.1 -P 3306 -u balrogadmin --password=balrogadmin -e "insert into permissions (username, permission, data_version) values (\"balrogadmin\", \"admin\", 0);" balrog'
 fi
 
 docker ps -a | grep -q balrog-admin
@@ -30,7 +30,7 @@ if [ "$?" == "0" ]; then
     echo "balrog-admin container already exists, skipping..."
 else
     echo "Creating balrog-admin container..."
-    docker create --net host -e DBURI=mysql://balrogadmin:balrogadmin@127.0.0.1/balrog -e SECRET_KEY=blahblah --name balrog-admin -v `pwd`:/app -it bhearsum/balrog:latest uwsgi --ini /app/uwsgi/admin.ini
+    docker create --net host -e DBURI=mysql://balrogadmin:balrogadmin@127.0.0.1/balrog -e SECRET_KEY=blahblah --name balrog-admin -v $balrog_repo:/app -it bhearsum/balrog:latest uwsgi --ini /app/uwsgi/admin.ini
     docker start balrog-admin
 fi
 
@@ -39,6 +39,6 @@ if [ "$?" == "0" ]; then
     echo "balrog-public container already exists, skipping..."
 else
     echo "Creating balrog-public container..."
-    docker create --net host -e DBURI=mysql://balrogadmin:balrogadmin@127.0.0.1/balrog --name balrog-public -v `pwd`:/app -it bhearsum/balrog:latest uwsgi --ini /app/uwsgi/public.ini
+    docker create --net host -e DBURI=mysql://balrogadmin:balrogadmin@127.0.0.1/balrog --name balrog-public -v $balrog_repo:/app -it bhearsum/balrog:latest uwsgi --ini /app/uwsgi/public.ini
     docker start balrog-public
 fi
