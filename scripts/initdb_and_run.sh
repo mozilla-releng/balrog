1506aad529725b9f12e97ea6cf4894e37730b29f
Rail Aliiev <rail@mozilla.com>
2016-02-12 09:47:16 -0500

    Add docker-compose support

---
+++ scripts/initdb_and_run.sh
@@ -0,0 +1,13 @@
+set -xe
+if [ ! -e /app/.cache/mysql/db.done ]; then
+    sleep 30
+    echo "Initializing DB..."
+    python scripts/manage-db.py -d mysql://balrogadmin:balrogadmin@balrogdb/balrog create
+    mysql -h balrogdb -u balrogadmin --password=balrogadmin balrog < /app/scripts/sample-data-permissions.sql
+    mysql -h balrogdb -u balrogadmin --password=balrogadmin -e "insert into permissions (username, permission, data_version) values (\"balrogadmin\", \"admin\", 1)" balrog
+    touch /app/.cache/mysql/db.done
+    echo "Done"
+fi
+
+# run the command passed from docker
+$@
