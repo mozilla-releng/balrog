4c059e6fc398ffe08bc40a6c1acb77a7e214c578
Rail Aliiev <rail@mozilla.com>
2016-02-16 11:27:39 -0800

    Use full data, delete old scripts

--- scripts/initdb_and_run.sh
+++ scripts/initdb_and_run.sh
@@ -3,7 +3,7 @@ if [ ! -e /app/.cache/mysql/db.done ]; then
     sleep 30
     echo "Initializing DB..."
     python scripts/manage-db.py -d mysql://balrogadmin:balrogadmin@balrogdb/balrog create
-    mysql -h balrogdb -u balrogadmin --password=balrogadmin balrog < /app/scripts/sample-data-permissions.sql
+    mysql -h balrogdb -u balrogadmin --password=balrogadmin balrog < /app/scripts/sample-data.sql
     mysql -h balrogdb -u balrogadmin --password=balrogadmin -e "insert into permissions (username, permission, data_version) values (\"balrogadmin\", \"admin\", 1)" balrog
     touch /app/.cache/mysql/db.done
     echo "Done"
