cdcb4f88c8e18c1ac3582f10b448abf70e654f23
Ben Hearsum <bhearsum@mozilla.com>
2016-02-29 15:51:52 -0500

    Update dockerhub credentials.

--- scripts/push-dockerimage.sh
+++ scripts/push-dockerimage.sh
@@ -3,8 +3,8 @@
 set -e
 
 password_url="taskcluster/secrets/v1/secret/repo:github.com/mozilla/balrog:dockerhub"
-dockerhub_email=bhearsum+test@gmail.com
-dockerhub_username=bhearsumtest
+dockerhub_email=release+balrog@mozilla.com
+dockerhub_username=mozillabalrog
 dockerhub_password=$(curl ${password_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["dockerhub_password"]')
 
 if [ -z $dockerhub_password ]; then
@@ -15,6 +15,6 @@ fi
 # TODO: create version.json
 # TODO: how can uninsntalled versions of library use version.json? maybe a stub one in the repo?
 
-docker build -t bhearsumtest/balrog:latest .
+docker build -t mozilla/balrog:latest .
 docker login -e $dockerhub_email -u $dockerhub_username -p $dockerhub_password
-docker push bhearsumtest/balrog:latest
+docker push mozilla/balrog:latest
