af886d89b359fb0e8db9689a55b533d8cb5a5126
Ben Hearsum <bhearsum@mozilla.com>
2016-04-20 16:57:27 -0400

    Use docker build --pull in test script to make sure we always use the most up-to-date version of the Python image. r=nthomas

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,5 @@
 
 # TODO: When we can run docker-compose in Taskcluster, we should use
 # docker-compose-test.yml instead of running docker directly.
-docker build -t balrogtest .
+docker build --pull -t balrogtest .
 docker run --entrypoint /app/scripts/test-entrypoint.sh balrogtest
