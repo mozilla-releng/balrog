7001d761a65a34b70d62cdda3a4c61bbb58dc251
Ben Hearsum <bhearsum@mozilla.com>
2016-04-22 09:42:27 -0400

    Strip away test code from new py35 client.

--- auslib/client/client_py35.py
+++ auslib/client/client_py35.py
@@ -1,4 +1,3 @@
-import asyncio
 import aiohttp
 import json
 import logging
@@ -23,7 +22,7 @@ async def request(session, api_root, path, method="GET", data=None, auth=None):
 
     if method not in ("GET", "HEAD"):
         # TODO: switch this to a HEAD after https://github.com/KeepSafe/aiohttp/issues/852 is released
-        async with session.request("GET", get_url(api_root, "/csrf_token"), auth=auth) as resp:
+        async with session.request("GET", get_url(api_root, "/csrf_token"), headers=headers, auth=auth) as resp:
             if resp.status in range(200, 300):
                 data["csrf_token"] = resp.headers["X-CSRF-Token"]
 
@@ -32,12 +31,3 @@ async def request(session, api_root, path, method="GET", data=None, auth=None):
             return await resp.json()
         else:
             raise Exception(resp.reason)
-
-
-async def change_rule(session):
-    res = await request(session, "http://localhost:8080/api", "/rules/69")
-    res["comment"] = "WTF"
-    return await request(session, "http://localhost:8080/api", "/rules/69", method="POST", data=res)
-
-with aiohttp.ClientSession(loop=loop) as session:
-    loop.run_until_complete(change_rule(session))
