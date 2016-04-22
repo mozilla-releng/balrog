12147212e46a744360af21cc61efae057e73f693
Ben Hearsum <bhearsum@mozilla.com>
2016-04-22 09:15:26 -0400

    Improve python version checking in client.

--- auslib/client/__init__.py
+++ auslib/client/__init__.py
@@ -1,10 +1,7 @@
+import logging
 import sys
 
-if sys.version_info.major == 2:
-    # No in-repo client for python 2 yet
-    pass
-elif sys.version_info.major == 3:
-    if sys.version_info.minor == 5:
-        from .client_py35 import *
-    else:
-        raise Exception("Python 3.5 is required.")
+if sys.version_info >= (3, 5):
+    from .client_py35 import *
+else:
+    logging.warning("No client available for this version of Python.")
