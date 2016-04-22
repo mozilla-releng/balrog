f085b31ec69e5f7bc7ab867546cab77f31485063
Ben Hearsum <bhearsum@mozilla.com>
2016-04-22 09:17:48 -0400

    Remove some debug code and don't require clients to know about aiohttp.BasicAuth.

--- auslib/client/client_py35.py
+++ auslib/client/client_py35.py
@@ -15,37 +15,23 @@ def get_url(api_root, path):
 
 
 async def request(session, api_root, path, method="GET", data=None, auth=None):
+    if auth:
+        auth = aiohttp.BasicAuth(*auth)
     url = get_url(api_root, path)
     if data:
         data = data.copy()
 
-    print(url)
-    print(method)
     if method not in ("GET", "HEAD"):
         # TODO: switch this to a HEAD after https://github.com/KeepSafe/aiohttp/issues/852 is released
         async with session.request("GET", get_url(api_root, "/csrf_token"), auth=auth) as resp:
-            print(resp.headers)
             if resp.status in range(200, 300):
                 data["csrf_token"] = resp.headers["X-CSRF-Token"]
 
-    print("doit")
     async with session.request(method, url, data=json.dumps(data), headers=headers, auth=auth) as resp:
         if resp.status in range(200, 300):
             return await resp.json()
         else:
             raise Exception(resp.reason)
-
-
-# get rule
-# modify rule
-# update rule
-
-loop = asyncio.get_event_loop()
-#with aiohttp.ClientSession(loop=loop) as session:
-#    fut = asyncio.ensure_future(request(session, "http://localhost:8080/api", "/rules/69"))
-#    fut.add_done_callback(lambda x: print("Here: {}", x.result()))
-#
-#    loop.run_until_complete(fut)
 
 
 async def change_rule(session):
