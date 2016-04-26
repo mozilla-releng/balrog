c65b9240c99e23f02914e10ce51fe0fab67a3b9e
Ben Hearsum <bhearsum@mozilla.com>
2016-04-26 09:30:42 -0400

    Replace sessions with optional loops.

--- agent/balrogagent/client.py
+++ agent/balrogagent/client.py
@@ -13,14 +13,14 @@ def get_url(api_root, path):
     return api_root.rstrip("/") + path
 
 
-async def request(session, api_root, path, method="GET", data=None, auth=None):
+async def request(api_root, path, method="GET", data=None, auth=None, loop=None):
     if auth:
         auth = aiohttp.BasicAuth(*auth)
     url = get_url(api_root, path)
     if data:
         data = data.copy()
 
-    async with session.request(method, url, data=json.dumps(data), headers=headers, auth=auth) as resp:
+    async with aiohttp.request(method, url, data=json.dumps(data), headers=headers, auth=auth, loop=loop) as resp:
         if resp.status in range(200, 300):
             return await resp.json()
         else:
