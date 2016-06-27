b15efd5a90681c7f1c33a454414210e84a3e3615
Ben Hearsum <bhearsum@mozilla.com>
2016-06-27 12:39:22 -0400

    Make cleanup action print out things to be deleted before it does so.

--- scripts/manage-db.py
+++ scripts/manage-db.py
@@ -13,6 +13,44 @@ sys.path.append(path.join(path.dirname(__file__), path.join("..", "vendor", "lib
 
 from auslib.db import AUSDatabase
 
+def cleanup_releases(trans, nightly_age, dryrun=True):
+    query = """
+LEFT OUTER JOIN releases_history USING (name)
+LEFT JOIN rules ON (name=mapping)
+WHERE name LIKE '%%%%nightly%%%%'
+AND (rules.whitelist <> releases.name OR rules.whitelist IS NULL)
+AND (rules.mapping <> releases.name OR rules.mapping IS NULL)
+AND (timestamp<1000*UNIX_TIMESTAMP(NOW()-INTERVAL %s MONTH) OR change_id is NULL);
+""" % nightly_age
+    if dryrun:
+        print "Releases rows to be deleted:"
+        print trans.execute("SELECT name FROM releases" + query).fetchall()
+    else:
+        trans.execute("DELETE releases FROM releases" + query)
+
+
+def cleanup_releases_history(trans, dryrun=True):
+    query = """
+WHERE name LIKE '%%%%latest%%%%'
+AND timestamp<1000*UNIX_TIMESTAMP(NOW()-INTERVAL 14 DAY);
+"""
+    if dryrun:
+        print "Releases history rows to be deleted:"
+        print trans.execute("SELECT name, change_id FROM releases_history" + query).fetchall()
+    else:
+        trans.execute("DELETE releases_history FROM releases_history")
+
+    query = """
+WHERE name NOT LIKE '%%%%latest%%%%' AND name LIKE '%%%%nightly%%%%'
+AND timestamp<1000*UNIX_TIMESTAMP(NOW()-INTERVAL 7 DAY);
+"""
+    if dryrun:
+        print "Releases history rows to be deleted:"
+        print trans.execute("SELECT name, change_id FROM releases_history" + query).fetchall()
+    else:
+        trans.execute("DELETE releases_history FROM releases_history")
+
+
 if __name__ == "__main__":
     from optparse import OptionParser
     usage = """%s --db dburi action [options]\n""" % sys.argv[0]
@@ -40,27 +78,13 @@ if __name__ == "__main__":
         db.upgrade(options.version)
     elif action == 'downgrade':
         db.downgrade(options.version)
-    elif action == "cleanup":
+    elif action.startswith("cleanup"):
         if len(args) < 2:
             parser.error("need to pass maximum nightly release age")
         nightly_age = int(args[1])
         with db.begin() as trans:
-            trans.execute("""
-DELETE releases FROM releases
-LEFT OUTER JOIN releases_history USING (name)
-LEFT JOIN rules ON (name=mapping)
-WHERE name LIKE '%%%%nightly%%%%'
-AND (rules.whitelist <> releases.name OR rules.whitelist IS NULL)
-AND (rules.mapping <> releases.name OR rules.mapping IS NULL)
-AND (timestamp<1000*UNIX_TIMESTAMP(NOW()-INTERVAL %s MONTH) OR change_id is NULL);
-""" % nightly_age)
-            trans.execute("""
-DELETE releases_history FROM releases_history
-WHERE name LIKE '%%%%latest%%%%'
-AND timestamp<1000*UNIX_TIMESTAMP(NOW()-INTERVAL 14 DAY);
-""")
-            trans.execute("""
-DELETE releases_history FROM releases_history
-WHERE name NOT LIKE '%%%%latest%%%%' AND name LIKE '%%%%nightly%%%%'
-AND timestamp<1000*UNIX_TIMESTAMP(NOW()-INTERVAL 7 DAY);
-""")
+            cleanup_releases(trans, nightly_age, dryrun=True)
+            cleanup_releases_history(trans, dryrun=True)
+            if action == "cleanup":
+                cleanup_releases(trans, nightly_age, dryrun=False)
+                cleanup_releases_history(trans, dryrun=False)
