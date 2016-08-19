3ace11305790406357f52e2dd4c0f99377097a8d
Ben Hearsum <bhearsum@mozilla.com>
2016-08-19 14:30:20 -0400

    Drop sessions to fix disconnect errors.

--- agent/balrogagent/client.py
+++ agent/balrogagent/client.py
@@ -1,3 +1,4 @@
+import aiohttp
 import json
 import logging
 
@@ -13,7 +14,7 @@ def get_url(api_root, path):
     return api_root.rstrip("/") + path
 
 
-async def request(session, api_root, path, method="GET", data={}, headers=default_headers):
+async def request(api_root, path, method="GET", data={}, headers=default_headers, auth=None, loop=None):
     headers = headers.copy()
     url = get_url(api_root, path)
     csrf_url = get_url(api_root, "/csrf_token")
@@ -21,17 +22,13 @@ async def request(session, api_root, path, method="GET", data={}, headers=defaul
 
     # TODO: sometimes getting "unclosed response" and "unclosed connection" errors.
     logging.debug("Sending %s request to %s", "HEAD", csrf_url)
-    resp = await session.request("HEAD", csrf_url)
+    resp = await aiohttp.request("HEAD", csrf_url, auth=auth, loop=loop)
     resp.raise_for_status()
     data["csrf_token"] = resp.headers["X-CSRF-Token"]
     resp.close()
 
     logging.debug("Sending %s request to %s", method, url)
-    resp = await session.request(method, url, data=json.dumps(data), headers=headers)
+    resp = await aiohttp.request(method, url, data=json.dumps(data), headers=headers, auth=auth, loop=loop)
     # Raises on 400 code or higher, we can assume things are good if we make it past this.
     resp.raise_for_status()
-    # TODO: is it okay that an async function is not returning a coroutine?
-    # We need this to be async so we can await the request above, but we
-    # want callers to have access to the raw response so they can check
-    # headers or stream the response if they choose.
     return resp
--- agent/balrogagent/cmd.py
+++ agent/balrogagent/cmd.py
@@ -14,36 +14,35 @@ async def run_agent(loop, balrog_api_root, balrog_username, balrog_password, tel
 
     while True:
         try:
-            async with aiohttp.ClientSession(auth=auth, loop=loop) as session:
-                logging.debug("Looking for active scheduled changes...")
-                resp = await client.request(session, balrog_api_root, "/scheduled_changes/rules")
-                sc = (await resp.json())["scheduled_changes"]
-                logging.debug("Found %s", len(sc))
-                for change in sc:
-                    logging.debug("Processing change %s", change["sc_id"])
-                    ready = False
+            logging.debug("Looking for active scheduled changes...")
+            resp = await client.request(balrog_api_root, "/scheduled_changes/rules", auth=auth, loop=loop)
+            sc = (await resp.json())["scheduled_changes"]
+            logging.debug("Found %s", len(sc))
+            for change in sc:
+                logging.debug("Processing change %s", change["sc_id"])
+                ready = False
 
-                    # Figure out if the change is ready, which is type-specific.
-                    if change["telemetry_uptake"]:
-                        # TODO: maybe replace this with a simple client.request()...
-                        current_uptake = await get_telemetry_uptake(change["telemetry_product"], change["telemetry_channel"], loop=loop)
-                        ready = telemetry_is_ready(change, current_uptake)
-                    elif change["when"]:
-                        # "when" is to-the-millisecond timestamp that gets stored as an int.
-                        # It needs to be converted back to a float before it can be compared
-                        # against other timestamps.
-                        ready = time_is_ready(change, time.time())
-                    else:
-                        logging.debug("Unknown change type!")
+                # Figure out if the change is ready, which is type-specific.
+                if change["telemetry_uptake"]:
+                    # TODO: maybe replace this with a simple client.request()...
+                    current_uptake = await get_telemetry_uptake(change["telemetry_product"], change["telemetry_channel"], loop=loop)
+                    ready = telemetry_is_ready(change, current_uptake)
+                elif change["when"]:
+                    # "when" is to-the-millisecond timestamp that gets stored as an int.
+                    # It needs to be converted back to a float before it can be compared
+                    # against other timestamps.
+                    ready = time_is_ready(change, time.time())
+                else:
+                    logging.debug("Unknown change type!")
 
-                    # If it *is* ready, enact it!
-                    if ready:
-                        logging.debug("Change %s is ready, enacting", change["sc_id"])
-                        endpoint = "/scheduled_changes/rules/{}/enact".format(change["sc_id"])
-                        resp = await client.request(session, balrog_api_root, endpoint, method="POST")
-                        resp.close()
-                    else:
-                        logging.debug("Change %s is not ready", change["sc_id"])
+                # If it *is* ready, enact it!
+                if ready:
+                    logging.debug("Change %s is ready, enacting", change["sc_id"])
+                    endpoint = "/scheduled_changes/rules/{}/enact".format(change["sc_id"])
+                    resp = await client.request(balrog_api_root, endpoint, method="POST", auth=auth, loop=loop)
+                    resp.close()
+                else:
+                    logging.debug("Change %s is not ready", change["sc_id"])
 
         except:
             logging.error(traceback.format_exc())
