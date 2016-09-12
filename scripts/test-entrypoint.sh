ecdf83e079af1f91ee5520c12a434f73db001191
Johan Lorenzo <jlorenzo@mozilla.com>
2016-09-12 19:05:29 +0200

    Bug 1299213 - Clean up ui/* now that no submodule is used (#115). r=bhearsum

--- scripts/test-entrypoint.sh
+++ scripts/test-entrypoint.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-pip install -r /app/requirements-test.txt
-tox $@
+run_back_end_tests() {
+  pip install -r /app/requirements-test.txt
+  tox $@
+}
 
-cd ui/
-npm test
+run_front_end_tests() {
+  cd ui/
+  npm test
+}
+
+type_of_tests="$1"
+
+if [[ $type_of_tests == "backend" ]]; then
+  shift
+  run_back_end_tests $@
+elif [[ $type_of_tests == "frontend" ]]; then
+  run_front_end_tests
+else
+  run_back_end_tests $@
+  run_front_end_tests
+fi
--- uwsgi/run.sh
+++ uwsgi/run.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+build_front_end() {
+    cd /app/ui
+    npm run build
+    cd -
+}
+
+
 if [ $1 == "public" ]; then
    exec uwsgi --ini /app/uwsgi/public.ini --python-autoreload 1
 elif [ $1 == "admin" ]; then
    exec uwsgi --ini /app/uwsgi/admin.ini --python-autoreload 1
 elif [ $1 == "admin-dev" ]; then
-   exec uwsgi --ini /app/uwsgi/admin.dev.ini --ini /app/uwsgi/admin.ini --python-autoreload 1
+    # When running the dev instance, we override /app by the developer's folder on his host
+    # machine. Building the frontend here makes sure we always serve the most up to date
+    # revision at each `docker-compose up`. You can still build it manually with, though
+    build_front_end
+
+    exec uwsgi --ini /app/uwsgi/admin.dev.ini --ini /app/uwsgi/admin.ini --python-autoreload 1
 elif [ $1 == "upgrade-db" ]; then
     if [ -z "${DBURI}" ]; then
         echo "\${DBURI} must be set!"
