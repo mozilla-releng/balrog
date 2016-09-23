09bd66609a61ec078f717d5577e15147b7e56b0a
Ben Hearsum <bhearsum@mozilla.com>
2016-09-23 17:01:15 -0400

    Add comment about non-obvious volume mount.

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,6 @@
 # TODO: When we can run docker-compose in Taskcluster, we should use
 # docker-compose-test.yml instead of running docker directly.
 docker build  -t balrogtest -f Dockerfile.dev .
+# Using a volume mount here ensures that the tox cache dir ends up on the host,
+# which avoids re-installing test dependencies every single time.
 docker run --rm -v `pwd`:/app --entrypoint /app/scripts/test-entrypoint.sh balrogtest $@
