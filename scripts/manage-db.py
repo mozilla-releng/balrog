fbcaf2b946acbc5beae06103789d3bdafc4dcc45
Ninad <bhat.ninadmb@gmail.com>
2016-09-27 19:39:11 +0530

    Initial Tempelate for review

--- scripts/manage-db.py
+++ scripts/manage-db.py
@@ -3,6 +3,7 @@
 import itertools
 import logging
 from os import path, popen
+import re
 import sys
 
 logging.basicConfig(level=logging.INFO)
@@ -84,11 +85,33 @@ def cleanup_releases_history(trans, dryrun=True):
 
     print "Total Deleted: %d" % total_deleted
 
-def extract_active_data( loc="."):
-    popen('mysqldump -h balrogdb -u balrogadmin -pbalrogadmin balrog rules rules_history rules_scheduled_changes \
-           rules_scheduled_changes_history permissions permissions_history migrate_version  > test.sql')
-    popen('mysqldump -h balrogdb -u balrogadmin -pbalrogadmin --single-transaction balrog releases \
-           --where="exists (select NULL from rules where releases.name = rules.mapping)" >> test.sql ')
+
+def extract_active_data(URL, loc="."):
+    """
+    Function was added in to enhance testing in Balrog's stage environment.
+
+    :param URL: Database. eg : mysql://balrogadmin:balrogadmin@balrogdb/balrog
+    :param loc: location where sqldump file must be created
+    :return: Stores sqldump data in dump.sql file in the specified location, if not specified stores in current directory
+            If dump.sql file already exists it will override that file and not append it.
+    """
+    data = re.split(':|/|@', URL)
+    user = data[3]
+    password = data[4]
+    host = data[5]
+    database = data[6]
+
+    popen('mysqldump -h %s -u %s -p%s --single-transaction %s rules rules_history rules_scheduled_changes \
+           rules_scheduled_changes_history permissions permissions_history migrate_version  > %s/dump.sql' % (host, user, password, database, loc,))
+    popen('mysqldump -h %s -u %s -p%s --single-transaction %s releases \
+           --where="exists (select * from rules where releases.name = rules.mapping)" >> %s/dump.sql ' % (host, user, password, database, loc,))
+
+    popen('mysqldump -h %s -u %s -p%s --single-transaction %s releases_history \
+    --where="releases_history.name=\'Firefox-mozilla-central-nightly-latest\' ">> %s/dump.sql' % (host, user, password, database, loc,))
+
+    popen('mysqldump -h %s -u %s -p%s --single-transaction %s releases_history --where="exists (select * from rules where \
+    releases_history.name = rules.mapping AND rules.alias LIKE \'firefox-release\' ) " >> %s/dump.sql' % (host, user, password, database, loc,))
+
 
 if __name__ == "__main__":
     from optparse import OptionParser
@@ -97,6 +120,7 @@ if __name__ == "__main__":
     usage += "  create: Create all the tables required for a new Balrog database\n"
     usage += "  upgrade: Upgrade an existing balrog table to a newer version.\n"
     usage += "  downgrade: Downgrade an existing balrog table to an older version.\n"
+    usage += "  extract: Extracts active data for testing "
     usage += "  cleanup: Cleanup old data from a database. Requires an extra arg of maximum age (in days) of nightly releases. " \
              "Anything older than this will be deleted.\n"
     usage += "  cleanup-dryrun: Show what would be removed if 'cleanup' is run."
@@ -119,8 +143,12 @@ if __name__ == "__main__":
         db.upgrade(options.version)
     elif action == 'downgrade':
         db.downgrade(options.version)
-    elif action == 'extra':
-        extract_active_data()
+    elif action == 'extract':
+        if len(args) < 2:
+            extract_active_data(options.db)
+        else:
+            location = args[1]
+            extract_active_data(options.db, location)
     elif action.startswith("cleanup"):
         if len(args) < 2:
             parser.error("need to pass maximum nightly release age")
