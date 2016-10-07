f203a6c533e69c40320432a3b5f9ba28d412b9fa
Ben Hearsum <bhearsum@mozilla.com>
2016-10-07 10:45:02 -0400

    Make sure python requirements are cached better in Dockerfile.dev

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,6 @@
 # TODO: When we can run docker-compose in Taskcluster, we should use
 # docker-compose-test.yml instead of running docker directly.
 docker build  -t balrogtest -f Dockerfile.dev .
-# Using a volume mount here ensures that the tox cache dir ends up on the host,
-# which avoids re-installing test dependencies every single time.
+# We can't use a volume mount here because this script is used for local
+# tests as well as CI, and Taskcluster doesn't support volume mounts.
 docker run --rm balrogtest test $@
