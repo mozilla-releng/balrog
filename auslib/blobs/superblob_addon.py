267a9c35f248da43e94202f09dbc7807418c2678
Ninad Bhat <bhat.ninadmb@gmail.com>
2016-10-12 01:28:59 +0530

    Adds superblob_addon.py

---
+++ auslib/blobs/superblob_addon.py
@@ -0,0 +1,21 @@
+from auslib.blobs.base import Blob
+
+
+class SuperBlobAddon(Blob):
+    jsonschema = "superblob_addon.yml"
+
+    def __init__(self, **kwargs):
+        Blob.__init__(self, **kwargs)
+        if "schema_version" not in self:
+            self["schema_version"] = 6000
+
+    def getResponseBlobs(self):
+        return self["blobs"]
+
+    def shouldServeUpdate(self, updateQuery):
+        # Since a superblob update will always be returned.
+        return True
+
+    def containsForbiddenDomain(self, product, whitelistedDomains):
+        # Since SuperBlobs don't have any URLs
+        return False
