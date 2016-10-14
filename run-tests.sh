27344e7546b965e775e0f558e8902912cdc92e9f
Ben Hearsum <bhearsum@mozilla.com>
2016-10-14 16:44:34 -0400

    Use volume mount when running tests in local dev, but not in Taskcluster.

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,13 @@
 # TODO: When we can run docker-compose in Taskcluster, we should use
 # docker-compose-test.yml instead of running docker directly.
 docker build  -t balrogtest -f Dockerfile.dev .
-# We can't use a volume mount here because this script is used for local
-# tests as well as CI, and Taskcluster doesn't support volume mounts.
-docker run --rm balrogtest test $@
+# We can't use a volume mount in Taskcluster, but we do want to use it
+# by default for local development, because it greatly speeds up repeated
+# test runs.
+if [ -n "${NO_VOLUME_MOUNT}" ]; then
+    echo "Running tests without volume mount"
+    docker run --rm balrogtest test $@
+else
+    echo "Running tests with volume mount"
+    docker run --rm -v `pwd`:/app balrogtest test $@
+fi
