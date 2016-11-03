f7c1d7b21b32ed7d17b374279c3c656b1cd1fe03
Ninad Bhat <bhat.ninadmb@gmail.com>
2016-11-03 13:46:29 +0530

    Correcting abspath

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/ninad/third_year/balrog'))
+sys.path.insert(0, os.path.abspath('../../'))
 autodoc_mock_imports = ['migrate.versioning.schema', 'migrate.versioning.api',
 			'dictdiffer', 'dictdiffer.merge', 'repoze.lru',
 		        'flask', 'flask.views', 'flask_wtf', 'wtforms',
