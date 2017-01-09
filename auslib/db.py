0f430a6c368172d5eb68ae8f07785fecd6efc553
Ben Hearsum <bhearsum@mozilla.com>
2017-01-09 16:29:50 -0500

    Remove TODO that has already been addressed.

--- auslib/db.py
+++ auslib/db.py
@@ -2061,7 +2061,6 @@ class Permissions(AUSTable):
             # XXX: This kindof sucks because it means that we don't have great control
             # over the signoffs required permissions that don't specify products, or
             # don't support them.
-            # TODO: select all required signoffs if product isn't specified
             if "products" in self.allPermissions[row["permission"]] and row.get("options") and row["options"].get("products"):
                 for product in row["options"]["products"]:
                     potential_signoffs.extend(self.db.permissionsRequiredSignoffs.select(where={"product": product}, transaction=transaction))
