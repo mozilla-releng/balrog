9b74632d4ac834a2c208bf6d6d050bbc9cc0f69a
Chris Hartjes <chartjes@littlehart.net>
2017-01-17 17:08:31 -0500

    Refactored to use external DTD files, updated DTD's to include optional parameters and empty responses

--- api-tests/test_update_responses.py
+++ api-tests/test_update_responses.py
@@ -1,62 +1,54 @@
+import pytest
 import requests
-from io import StringIO
 from lxml import etree
 
+def validate_responses_helper(dtd, urls):
+    for url in urls:
+        response = requests.get(url)
+        root = etree.XML(response.text)
+        valid = dtd.validate(root)
 
-def test_update_addon_responses():
+        if valid:
+            assert True
+        else:
+            print(dtd.error_log.filter_from_errors())
+            assert False
+
+
+def test_update_addon_responses(server_url):
+    print(server_url)
     addon_urls = [
-        'https://aus5.mozilla.org/update/3/SystemAddons/44.0/20160310153207/Darwin_x86_64-gcc3-u-i386-x86_64/en-GB/release/Darwin%2014.5.0/default/default/update.xml',
+        server_url + "/update/3/SystemAddons/44.0/20160310153207/Darwin_x86_64-gcc3-u-i386-x86_64/en-GB/release/Darwin%2014.5.0/default/default/update.xml",
     ]
-    dtd_info = """
-    <!ELEMENT updates (addons) >
-    <!ELEMENT addons (addon+) >
-    <!ELEMENT addon (#PCDATA)>
-    <!ATTLIST addon
-    id CDATA #REQUIRED
-    URL CDATA #REQUIRED
-    hashFunction CDATA #REQUIRED
-    hashValue CDATA #REQUIRED
-    size CDATA #REQUIRED
-    version CDATA #REQUIRED >
-    """
-    f = StringIO(dtd_info)
+    f = open('./api-tests/addons.dtd')
     dtd = etree.DTD(f)
-
-    for url in addon_urls:
-        response = requests.get(url)
-        root = etree.XML(response.text)
-        assert dtd.validate(root)
+    validate_responses_helper(dtd, addon_urls)
 
 
-def test_update_patch_responses():
+def test_update_patch_responses(server_url):
     patch_urls = [
-        'https://aus5.mozilla.org/update/3/Firefox/50.0.1/20161123182536/WINNT_x86_64-msvc-x64/en-US/release/default/default/default/update.xml?force=1',
-        'https://aus5.mozilla.org/update/3/Firefox/50.0.1/20161123182536/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/default/default/default/update.xml?force=1',
-        'https://aus5.mozilla.org/update/3/Firefox/50.0.1/20161123182536/Linux_x86_64-gcc3/en-US/release/default/default/default/update.xml?force=1',
+        server_url + '/update/3/Firefox/50.0.1/20161123182536/WINNT_x86_64-msvc-x64/en-US/release/default/default/default/update.xml?force=1',
+        server_url + '/update/3/Firefox/50.0.1/20161123182536/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/default/default/default/update.xml?force=1',
+        server_url + '/update/3/Firefox/50.0.1/20161123182536/Linux_x86_64-gcc3/en-US/release/default/default/default/update.xml?force=1',
     ]
-
-    dtd_info = """
-    <!ELEMENT updates (update) >
-    <!ELEMENT update (patch+) >
-    <!ATTLIST update
-    type CDATA #REQUIRED
-    displayVersion CDATA #REQUIRED
-    appVersion CDATA #REQUIRED
-    platformVersion CDATA #REQUIRED
-    buildID CDATA #REQUIRED
-    detailsURL CDATA #REQUIRED >
-    <!ELEMENT patch (#PCDATA)>
-    <!ATTLIST patch
-    type CDATA #REQUIRED
-    URL CDATA #REQUIRED
-    hashFunction CDATA #REQUIRED
-    hashValue CDATA #REQUIRED
-    size CDATA #REQUIRED >
-    """
-    f = StringIO(dtd_info)
+    f = open('./api-tests/updates.dtd')
     dtd = etree.DTD(f)
+    validate_responses_helper(dtd, patch_urls)
 
-    for url in patch_urls:
-        response = requests.get(url)
-        root = etree.XML(response.text)
-        assert dtd.validate(root)
+
+def test_update_with_no_addons():
+    xml = '''
+    <updates>
+        <addons> </addons>
+    </updates>
+    '''
+    f = open('./api-tests/addons.dtd')
+    dtd = etree.DTD(f)
+    root = etree.XML(xml)
+    valid = dtd.validate(root)
+
+    if valid:
+        assert True
+    else:
+        print(dtd.error_log.filter_from_errors())
+        assert False
