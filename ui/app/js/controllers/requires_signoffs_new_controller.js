8d11edcf06c83546c86a9f6eadc8501aa47f8593
Ben Hearsum <bhearsum@mozilla.com>
2017-01-24 16:52:50 -0500

    Make improvements to the layout and title.

--- ui/app/js/controllers/requires_signoffs_new_controller.js
+++ ui/app/js/controllers/requires_signoffs_new_controller.js
@@ -1,15 +1,36 @@
 angular.module("app").controller("NewRequiredSignoffCtrl",
 function($scope, $modalInstance, CSRF, ProductRequiredSignoffs, PermissionsRequiredSignoffs) {
+  $scope.saving = false;
+  $scope.errors = {};
+
+  $scope.mode = "channel";
   $scope.product = "";
   $scope.channel = "";
   $scope.new_roles = 1;
 
+  $scope.getTitle = function () {
+    var title = "Signoff Requirements";
+    if ($scope.product !== "") {
+      if ($scope.mode === "channel" && $scope.channel !== "") {
+        title += " for the " + $scope.product + " " + $scope.channel + " channel";
+      }
+      else if ($scope.mode === "permissions") {
+        title += " for " + $scope.product + " Permissions";
+      }
+    }
+    return title;
+  };
   $scope.addRole = function() {
     $scope.new_roles++;
   };
 
   $scope.saveChanges = function() {
+    $scope.saving = true;
+    $scope.errors = {};
 
+    CSRF.getToken()
+    .then(function(csrf_token) {
+    });
   };
 
   $scope.cancel = function() {
