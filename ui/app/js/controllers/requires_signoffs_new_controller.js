e61931b24367a9857e54069600fc48ed4cadc157
Ben Hearsum <bhearsum@mozilla.com>
2017-01-25 11:08:33 -0500

    Rework new Service to be inheritable; MVP for adding the first required signoff for a product.

--- ui/app/js/controllers/requires_signoffs_new_controller.js
+++ ui/app/js/controllers/requires_signoffs_new_controller.js
@@ -25,27 +25,70 @@ function($scope, $modalInstance, CSRF, ProductRequiredSignoffs, PermissionsRequi
     $scope.saving = true;
     $scope.errors = {};
 
-    var roles = {};
+    var required_signoffs = {};
 
-    // collect all of the new roles and signoff requirements
+    // Collect all of the new roles and signoff requirements
     $("#new_roles").find("tr").each(function(index, rs) {
-    //for (var rs in $("#new_roles").find("tr")) {
       rs = $(rs);
-      console.log(rs.find("input[name='role']")[0].value);
-      console.log(rs.find("input[name='signoffs_required']")[0].value);
-    });
-
-    CSRF.getToken()
-    .then(function(csrf_token) {
-      for (var i = 0; i < $scope.new_roles; i++) {
-        if ($scope.mode === "channel") {
-          if (i === 0) {
-          }
-        }
-        else if ($scope.mode === "permissions") {
-        }
+      var role = rs.find("input[name='role']")[0].value;
+      var signoffs_required = rs.find("input[name='signoffs_required']")[0].value;
+      if (role in required_signoffs) {
+        $scope.errors["role"] = "Cannot specify any Role more than once.";
+      }
+      else {
+        required_signoffs[role] = signoffs_required;
       }
     });
+
+    console.log($scope.errors);
+    console.log(required_signoffs);
+
+    if (Object.keys($scope.errors).length === 0) {
+      CSRF.getToken()
+      .then(function(csrf_token) {
+        var service = null;
+        var first = true;
+
+        if ($scope.mode === "channel") {
+          service = ProductRequiredSignoffs;
+        }
+        else if ($scope.mode === "permissions") {
+          service = PermissionsRequiredSignoffs;
+        }
+        else {
+          $scope.errors["exception"] = "Couldn't detect mode";
+          $scope.saving = false;
+        }
+
+        var successCallback = function(response) {
+          // TODO
+        };
+        var errorCallback = function(response, status) {
+          // TODO
+        };
+
+        for (var role in required_signoffs) {
+          var data = {"product": $scope.product, "role": role, "signoffs_required": required_signoffs[role], "csrf_token": csrf_token};
+          if ($scope.mode === "channel") {
+            data["channel"] = $scope.channel;
+          }
+          if (first) {
+            first = false;
+
+            service.addRequiredSignoff(data)
+            .success(successCallback)
+            .error(errorCallback);
+          }
+          else {
+            // todo: add rs stuff to data
+            service.addScheduledChange(data)
+            .success(successCallback)
+            .error(errorCallback);
+          }
+        }
+      });
+    }
+    // todo: this probably isn't getting set in exception cases
     $scope.saving = false;
   };
 
--- ui/app/js/services/product_required_signoffs.js
+++ ui/app/js/services/product_required_signoffs.js
@@ -1,12 +1,24 @@
-angular.module("app").factory("ProductRequiredSignoffs", function($http, $q) {
+// Inheritance model inspired by:
+// http://blog.mgechev.com/2013/12/18/inheritance-services-controllers-in-angularjs/
+// http://davidshariff.com/blog/javascript-inheritance-patterns/
+
+var RequiredSignoffBase = (function() {
   var service = {
     getRequiredSignoffs: function() {
-      return $http.get("/api/required_signoffs/product");
+      var uri = this.base_uri;
+      return this.http.get(uri);
     },
-    addRequiredSignoff: function(data, csrf_token) {
-      data.csrf_token = csrf_token;
-      return $http.post("/api/required_signoffs/product", data);
+    addRequiredSignoff: function(data) {
+      var uri = this.base_uri;
+      return this.http.post(uri, data);
     },
   };
+  return service;
+}());
+
+angular.module("app").factory("ProductRequiredSignoffs", function($http) {
+  var service = Object.create(RequiredSignoffBase);
+  service.base_uri = "/api/required_signoffs/product";
+  service.http = $http;
   return service;
 });
