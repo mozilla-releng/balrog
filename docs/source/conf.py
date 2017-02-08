918aadb6190ee3ee8c11408ba7f40effb5f1cc7f
Nick Thomas <nrthomas@gmail.com>
2017-02-08 17:44:56 +1300

    Moar docs for rule resolution

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ here = path.abspath(path.dirname(__file__))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.ifconfig', 'sphinx.ext.autodoc'
+    'sphinx.ext.ifconfig', 'sphinx.ext.autodoc', 'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
