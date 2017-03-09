4fc79802cece8223df52748a20afbedd654a3c8d
tyagi-iiitv <anjul.ten@gmail.com>
2017-03-09 23:35:18 +0530

    Trying out stuff..

--- auslib/test/web/test_dockerflow.py
+++ auslib/test/web/test_dockerflow.py
@@ -1,9 +1,9 @@
 import mock
 
-from auslib.test.admin.views.base import ViewTest
+from auslib.test.web.test_client import ClientTestBase
 
 
-class TestDockerflowEndpoints(ViewTest):
+class TestDockerflowEndpoints(ClientTestBase):
 
     def testVersion(self):
         ret = self.client.get("/__version__")
@@ -16,22 +16,18 @@ class TestDockerflowEndpoints(ViewTest):
 """)
 
     def testHeartbeat(self):
-        with mock.patch("auslib.global_state.dbo.dockerflow.incrementWatchdogValue") as cr:
-            cr.side_effect = (1, 2, 3)
-            for i in range(1, 3):
-                ret = self.client.get("/__heartbeat__")
-                self.assertEqual(ret.status_code, 200)
-                self.assertEqual(cr.call_count, i)
-                self.assertEqual(ret.headers["Cache-Control"], "no-cache")
-                returned_digit = int(ret.data)
-                self.assertEqual(returned_digit, i)
+        with mock.patch("auslib.global_state.dbo.rules.countRules") as cr:
+            ret = self.client.get("/__heartbeat__")
+            self.assertEqual(ret.status_code, 200)
+            self.assertEqual(cr.call_count, 1)
+            self.assertEqual(ret.headers["Cache-Control"], "no-cache")
 
     def testHeartbeatWithException(self):
-        with mock.patch("auslib.global_state.dbo.dockerflow.incrementWatchdogValue") as cr:
+        with mock.patch("auslib.global_state.dbo.rules.countRules") as cr:
             cr.side_effect = Exception("kabom!")
             # Because there's no web server between us and the endpoint, we receive
             # the Exception directly instead of a 500 error
-            ret = self.assertRaises(Exception, self.client.get, "/__heartbeat__")
+            self.assertRaises(Exception, self.client.get, "/__heartbeat__")
             self.assertEqual(cr.call_count, 1)
 
     def testLbHeartbeat(self):
