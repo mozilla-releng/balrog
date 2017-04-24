ba08145cb6bf734b5fe182896b46b62307d103f1
Dilek Uzulmez <dilekuzulmez@gmail.com>
2017-04-24 15:44:14 +0300

    Delete Release Scheduled functions on Release Scheduled Change Data Controll

--- ui/app/js/controllers/release_scheduled_change_data_controller.js
+++ ui/app/js/controllers/release_scheduled_change_data_controller.js
@@ -3,21 +3,8 @@ function($scope, $http, $modalInstance, Releases, Rules, release, diff) {
   $scope.release = release;
   $scope.diff = diff;
 
-  if (release.change_id) {
-    if (diff) {
-      Releases.getDiff(release.change_id)
-      .then(function(response) {
-        $scope.release.diff = response.data;
-      });
+   
 
-    } else {
-      Releases.getData(release.change_id)
-      .then(function(response) {
-        $scope.release.data = response.data;
-      });
-    }
-
-  }  
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
