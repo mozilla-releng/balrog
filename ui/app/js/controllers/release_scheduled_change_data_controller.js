847c15f122bf2628dc5e3c7f5f4c03290127981d
Dilek Uzulmez <dilekuzulmez@gmail.com>
2017-05-17 18:12:11 +0300

    Fixed '===' instead of '=='

--- ui/app/js/controllers/release_scheduled_change_data_controller.js
+++ ui/app/js/controllers/release_scheduled_change_data_controller.js
@@ -4,7 +4,7 @@ function($scope, $http, $modalInstance, Releases, Rules, release, diff) {
   $scope.diff = diff;
 
   
-  if(release.change_type=="delete"){
+  if(release.change_type==="delete"){
   	Releases.getRelease(release.name)
       .then(function(response) {
         $scope.release.data = response.data;
