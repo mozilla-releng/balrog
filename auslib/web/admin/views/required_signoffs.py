426127c3a1108eb90b0da94972fd3d80e1044682
Ashish Sareen <ashish.sareen95@gmail.com>
2017-06-24 02:22:40 +0530

    Bug 1336452: convert Required Signoffs APIs to swagger (#342). r=bhearsum

--- auslib/web/admin/views/required_signoffs.py
+++ auslib/web/admin/views/required_signoffs.py
@@ -1,25 +1,24 @@
 import json
+import connexion
 
 from sqlalchemy.sql.expression import null
 
-from flask import jsonify, Response, request
+from flask import jsonify, Response
 
 from auslib.web.admin.views.base import requirelogin, AdminView
-from auslib.web.admin.views.forms import ProductRequiredSignoffForm, \
-    ProductRequiredSignoffHistoryForm, \
+from auslib.web.admin.views.forms import \
     ScheduledChangeExistingProductRequiredSignoffForm, \
     ScheduledChangeNewProductRequiredSignoffForm, \
     ScheduledChangeDeleteProductRequiredSignoffForm, \
     EditScheduledChangeNewProductRequiredSignoffForm, \
     EditScheduledChangeExistingProductRequiredSignoffForm, \
-    PermissionsRequiredSignoffForm, \
-    PermissionsRequiredSignoffHistoryForm, \
     ScheduledChangeExistingPermissionsRequiredSignoffForm, \
     ScheduledChangeNewPermissionsRequiredSignoffForm, \
     ScheduledChangeDeletePermissionsRequiredSignoffForm, \
     EditScheduledChangeNewPermissionsRequiredSignoffForm, \
     EditScheduledChangeExistingPermissionsRequiredSignoffForm
 from auslib.web.admin.views.history import HistoryView
+from auslib.web.admin.views.problem import problem
 from auslib.web.admin.views.scheduled_changes import ScheduledChangesView, \
     ScheduledChangeView, EnactScheduledChangeView, SignoffsView, \
     ScheduledChangeHistoryView
@@ -38,21 +37,17 @@ class RequiredSignoffsView(AdminView):
         rows = self.table.select()
         return jsonify({"count": len(rows), "required_signoffs": [dict(rs) for rs in rows]})
 
-    def _post(self, form, transaction, changed_by):
-        if not form.validate():
-            self.log.warning("Bad input: %s", form.errors)
-            return Response(status=400, response=json.dumps(form.errors))
-
-        where = {f: form[f].data for f in self.decisionFields}
+    def _post(self, what, transaction, changed_by):
+        where = {f: what.get(f) for f in self.decisionFields}
         if self.table.select(where=where, transaction=transaction):
             raise SignoffRequiredError("Required Signoffs cannot be directly modified")
         else:
             try:
-                self.table.insert(changed_by=changed_by, transaction=transaction, **form.data)
+                self.table.insert(changed_by=changed_by, transaction=transaction, **what)
                 return Response(status=201, response=json.dumps({"new_data_version": 1}))
             except ValueError as e:
                 self.log.warning("Bad input: %s", e.args)
-                return Response(status=400, response=e.args)
+                return problem(400, "Bad Request", str(e.args))
 
     def _delete(self, *args, **kwargs):
         raise SignoffRequiredError("Required Signoffs cannot be directly deleted.")
@@ -64,25 +59,24 @@ class RequiredSignoffsHistoryAPIView(HistoryView):
         self.decisionFields = decisionFields
         super(RequiredSignoffsHistoryAPIView, self).__init__(table=table)
 
-    def get(self, form):
-        if not self.table.select({f: getattr(form, f).data for f in self.decisionFields}):
-            return Response(status=404, response="Requested Required Signoff does not exist")
+    def get(self, input_dict):
+        if not self.table.select({f: input_dict.get(f) for f in self.decisionFields}):
+            return problem(404, "Not Found", "Requested Required Signoff does not exist")
 
         try:
-            page = int(request.args.get('page', 1))
-            limit = int(request.args.get('limit', 100))
-            assert page >= 1
-        except (ValueError, AssertionError) as msg:
+            page = int(connexion.request.args.get('page', 1))
+            limit = int(connexion.request.args.get('limit', 100))
+        except ValueError as msg:
             self.log.warning("Bad input: %s", msg)
-            return Response(status=400, response=str(msg))
+            return problem(400, "Bad Request", str(msg))
         offset = limit * (page - 1)
 
         query = self.table.history.t.count().where(self.table.history.data_version != null())
         for field in self.decisionFields:
-            query = query.where(getattr(self.table.history, field) == getattr(form, field).data)
+            query = query.where(getattr(self.table.history, field) == input_dict.get(field))
         total_count = query.execute().fetchone()[0]
 
-        where = [getattr(self.table.history, f) == getattr(form, f).data for f in self.decisionFields]
+        where = [getattr(self.table.history, f) == input_dict.get(f) for f in self.decisionFields]
         where.append(self.table.history.data_version != null())
         revisions = self.table.history.select(
             where=where, limit=limit, offset=offset,
@@ -93,24 +87,32 @@ class RequiredSignoffsHistoryAPIView(HistoryView):
 
 
 class ProductRequiredSignoffsView(RequiredSignoffsView):
+    """/required_signoffs/product"""
 
     def __init__(self):
         super(ProductRequiredSignoffsView, self).__init__(dbo.productRequiredSignoffs, ["product", "channel", "role"])
 
     @requirelogin
     def _post(self, transaction, changed_by):
-        form = ProductRequiredSignoffForm()
-        return super(ProductRequiredSignoffsView, self)._post(form, transaction, changed_by)
+        what = {"product": connexion.request.json.get("product"),
+                "channel": connexion.request.json.get("channel"),
+                "role": connexion.request.json.get("role"),
+                "signoffs_required": int(connexion.request.json.get("signoffs_required")),
+                }
+        return super(ProductRequiredSignoffsView, self)._post(what, transaction, changed_by)
 
 
 class ProductRequiredSignoffsHistoryAPIView(RequiredSignoffsHistoryAPIView):
+    """/required_signoffs/product/revisions"""
 
     def __init__(self):
         super(ProductRequiredSignoffsHistoryAPIView, self).__init__(dbo.productRequiredSignoffs, ["product", "channel", "role"])
 
     def get(self):
-        form = ProductRequiredSignoffHistoryForm(request.args)
-        return super(ProductRequiredSignoffsHistoryAPIView, self).get(form)
+        input_dict = {'product': connexion.request.args.get('product'),
+                      'role': connexion.request.args.get('role'),
+                      'channel': connexion.request.args.get('channel')}
+        return super(ProductRequiredSignoffsHistoryAPIView, self).get(input_dict)
 
 
 class ProductRequiredSignoffsScheduledChangesView(ScheduledChangesView):
@@ -119,7 +121,7 @@ class ProductRequiredSignoffsScheduledChangesView(ScheduledChangesView):
 
     @requirelogin
     def _post(self, transaction, changed_by):
-        change_type = request.json.get("change_type")
+        change_type = connexion.request.json.get("change_type")
 
         if change_type == "update":
             form = ScheduledChangeExistingProductRequiredSignoffForm()
@@ -140,7 +142,7 @@ class ProductRequiredSignoffScheduledChangeView(ScheduledChangeView):
 
     @requirelogin
     def _post(self, sc_id, transaction, changed_by):
-        if request.json and request.json.get("data_version"):
+        if connexion.request.json and connexion.request.json.get("data_version"):
             form = EditScheduledChangeExistingProductRequiredSignoffForm()
         else:
             form = EditScheduledChangeNewProductRequiredSignoffForm()
@@ -176,24 +178,29 @@ class ProductRequiredSignoffScheduledChangeHistoryView(ScheduledChangeHistoryVie
 
 
 class PermissionsRequiredSignoffsView(RequiredSignoffsView):
+    """/required_signoffs/permissions"""
 
     def __init__(self):
         super(PermissionsRequiredSignoffsView, self).__init__(dbo.permissionsRequiredSignoffs, ["product", "role"])
 
     @requirelogin
     def _post(self, transaction, changed_by):
-        form = PermissionsRequiredSignoffForm()
-        return super(PermissionsRequiredSignoffsView, self)._post(form, transaction, changed_by)
+        what = {"product": connexion.request.json.get("product"),
+                "role": connexion.request.json.get("role"),
+                "signoffs_required": int(connexion.request.json.get("signoffs_required")),
+                }
+        return super(PermissionsRequiredSignoffsView, self)._post(what, transaction, changed_by)
 
 
 class PermissionsRequiredSignoffsHistoryAPIView(RequiredSignoffsHistoryAPIView):
+    """/required_signoffs/permissions/revisions"""
 
     def __init__(self):
         super(PermissionsRequiredSignoffsHistoryAPIView, self).__init__(dbo.permissionsRequiredSignoffs, ["product", "role"])
 
     def get(self):
-        form = PermissionsRequiredSignoffHistoryForm(request.args)
-        return super(PermissionsRequiredSignoffsHistoryAPIView, self).get(form)
+        input_dict = {'product': connexion.request.args.get('product'), 'role': connexion.request.args.get('role')}
+        return super(PermissionsRequiredSignoffsHistoryAPIView, self).get(input_dict)
 
 
 class PermissionsRequiredSignoffsScheduledChangesView(ScheduledChangesView):
@@ -202,7 +209,7 @@ class PermissionsRequiredSignoffsScheduledChangesView(ScheduledChangesView):
 
     @requirelogin
     def _post(self, transaction, changed_by):
-        change_type = request.json.get("change_type")
+        change_type = connexion.request.json.get("change_type")
 
         if change_type == "update":
             form = ScheduledChangeExistingPermissionsRequiredSignoffForm()
@@ -223,7 +230,7 @@ class PermissionsRequiredSignoffScheduledChangeView(ScheduledChangeView):
 
     @requirelogin
     def _post(self, sc_id, transaction, changed_by):
-        if request.json and request.json.get("data_version"):
+        if connexion.request.json and connexion.request.json.get("data_version"):
             form = EditScheduledChangeExistingPermissionsRequiredSignoffForm()
         else:
             form = EditScheduledChangeNewPermissionsRequiredSignoffForm()
