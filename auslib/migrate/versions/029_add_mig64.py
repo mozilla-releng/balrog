14251410cba2b579548c49ee5f634a9c50c9bb11
Ben Hearsum <bhearsum@mozilla.com>
2017-08-16 13:06:52 -0400

    Fix upgrade script and a UI display glitch.

--- auslib/migrate/versions/029_add_mig64.py
+++ auslib/migrate/versions/029_add_mig64.py
@@ -15,8 +15,8 @@ def upgrade(migrate_engine):
     base_mig64 = Column("base_mig64", CompatibleBooleanColumn)
     base_mig64.create(Table("rules_scheduled_changes", metadata, autoload=True))
 
-    base_mig64 = Column("base_mig64", CompatibleBooleanColumn)
-    base_mig64.create(Table("rules_scheduled_changes_history", metadata, autoload=True))
+    history_base_mig64 = Column("base_mig64", CompatibleBooleanColumn)
+    history_base_mig64.create(Table("rules_scheduled_changes_history", metadata, autoload=True))
 
 
 def downgrade(migrate_engine):
