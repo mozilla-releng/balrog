e12f424d6e560966d1c626d3dcf24accf66973d5
Ben Hearsum <bhearsum@mozilla.com>
2017-08-24 10:41:15 -0400

    Fix rule_field directive to use objects instead of strings.

--- ui/app/js/directives/rule_field.js
+++ ui/app/js/directives/rule_field.js
@@ -4,17 +4,14 @@ angular.module("app").directive("rulefield", function() {
   return {
     restrict: "E",
     scope: {
-      showsc: "@",
-      rule: "@",
+      showsc: "=",
+      rule: "=",
       fieldname: "@",
       fieldtitle: "@",
     },
     templateUrl: "rule_field.html",
     controller: function($scope) {
       $scope.sc = $scope.rule.scheduled_change;
-      $scope.showsc = ($scope.showsc === 'true');
-      $scope.showsc = true;
-      console.log($scope);
     }
   };
 });
