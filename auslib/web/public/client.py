e5c555e4de92ef333bcc60131b9e957a4b73ebe8
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-08-29 22:17:44 -0400

    Change API so that force=-1 means "force failure"

--- auslib/web/public/client.py
+++ auslib/web/public/client.py
@@ -86,7 +86,7 @@ def getQueryFromURL(url):
     ua = request.headers.get('User-Agent')
     query['headerArchitecture'] = getHeaderArchitecture(query['buildTarget'], ua)
     force = query.get('force')
-    query['force'] = {'1': SUCCEED, '0': FAIL}.get(force)
+    query['force'] = {'1': SUCCEED, '-1': FAIL}.get(force)
     if "mig64" in query:
         # "1" is the only value that official clients send. We ignore any other values
         # by setting mig64 to None.
