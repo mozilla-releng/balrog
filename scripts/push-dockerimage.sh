3b4a2fd72fa4ab1f015e8d1d5fabf754a015d5e3
Ben Hearsum <bhearsum@mozilla.com>
2017-09-21 10:33:47 -0400

    bug 1398237: adjust release process for new stage set-up (#399). r=rail

--- scripts/push-dockerimage.sh
+++ scripts/push-dockerimage.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+extra_tag=$1
+
 password_url="taskcluster/secrets/v1/secret/repo:github.com/mozilla/balrog:dockerhub"
 artifact_url="taskcluster/queue/v1/task/${TASK_ID}/runs/${RUN_ID}/artifacts/public/docker-image-shasum256.txt"
 artifact_expiry=$(date -d "+1 year" -u +%FT%TZ)
@@ -16,7 +18,11 @@ fi
 
 commit=$(git rev-parse HEAD)
 version=$(cat version.txt)
-branch=$(git rev-parse --abbrev-ref HEAD)
+# This is hardcoded because we can't accurately set it programatically for
+# release events, where we've updated to a tag. At the time of writing,
+# we only built docker images for commits to master or release events...
+branch=master
+#branch=$(git rev-parse --abbrev-ref HEAD)
 date=$(date --utc +%Y-%m-%d-%H-%M)
 
 cat > version.json <<EOF
@@ -28,31 +34,29 @@ cat > version.json <<EOF
 }
 EOF
 
-# Initialize and update the UI submodule
-git submodule init
-git submodule update
-
 branch_tag="${branch}"
 if [ "$branch" == "master" ]; then
     branch_tag="latest"
 fi
-date_tag="${branch}-${date}"
 commit_tag="${branch}-${commit}"
 
 echo "Building Docker image"
 docker build -t mozilla/balrog:${branch_tag} .
-echo "Tagging Docker image with date tag"
-docker tag mozilla/balrog:${branch_tag} "mozilla/balrog:${date_tag}"
 echo "Tagging Docker image with git commit tag"
 docker tag mozilla/balrog:${branch_tag} "mozilla/balrog:${commit_tag}"
 echo "Logging into Dockerhub"
 docker login -e $dockerhub_email -u $dockerhub_username -p $dockerhub_password
 echo "Pushing Docker image"
 docker push mozilla/balrog:${branch_tag}
-docker push mozilla/balrog:${date_tag}
 docker push mozilla/balrog:${commit_tag}
 
-sha256=$(docker images --no-trunc mozilla/balrog | grep "${date_tag}" | awk '/^mozilla/ {print $3}')
+if [ ! -z $extra_tag ]; then
+  echo "Tagging Docker image with ${extra_tag}"
+  docker tag mozilla/balrog:${branch_tag} "mozilla/balrog:${extra_tag}"
+  docker push mozilla/balrog:${extra_tag}
+fi
+
+sha256=$(docker images --no-trunc mozilla/balrog | grep "${commit_tag}" | awk '/^mozilla/ {print $3}')
 echo "SHA256 is ${sha256}, creating artifact for it"
 put_url=$(curl --retry 5 --retry-delay 5 --data "{\"storageType\": \"s3\", \"contentType\": \"text/plain\", \"expires\": \"${artifact_expiry}\"}" ${artifact_url} | python -c 'import json; import sys; print json.load(sys.stdin)["putUrl"]')
 curl --retry 5 --retry-delay 5 -X PUT -H "Content-Type: text/plain" --data "${sha256}" "${put_url}"
