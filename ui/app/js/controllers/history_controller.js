04fd84a7255d7b0c316a0d46a7f3dbd3dc149dee
Hope Ngerebara <hope.ngerebara@andela.com>
2017-09-22 07:19:59 +0100

    UI improvement: add tabs

--- ui/app/js/controllers/history_controller.js
+++ ui/app/js/controllers/history_controller.js
@@ -1,20 +1,37 @@
 angular.module("app").controller('HistoryController',
-function($scope, Page) {
+function($scope, Releases, Page) {
     Page.setTitle('History');
     
-    $scope.filter = [
-        {username: ""},
-        {timestamp: ""},
-        {product_channel: ""}
-    ];
-    // $scope.username = "";
-    // $scope.timestamp = "";
-    // $scope.product_channel = "";
+    $scope.tab = 1;
+    $scope.rowtab = "#rulesHistory";
+    $scope.filter = [];
+    $scope.username = "";
+    $scope.timestamp = "";
+    $scope.product_channel = "";
+
+    $scope.setTab = function(newTab){
+        $scope.tab = newTab;
+    };
+
+    $scope.isSet = function(tabNum){
+        return $scope.tab === tabNum;
+    };
+
+    $scope.tabChange = function(e){
+        if (e.target.nodeName === 'A') {
+            $scope.rowtab = e.target.getAttribute("href");
+            e.preventDefault();
+        }
+    }
 
     $scope.$watch('filtering_str', function(value) {
         $scope.filtering = value.value.split(',');
       });
     $scope.filtering_options = [
+        {
+        text: "All",
+        value: "default"
+        },
         {
         text: "Username / Email",
         value: "username_email"
@@ -29,6 +46,38 @@ function($scope, Page) {
         },
     ];
     $scope.filtering_str = $scope.filtering_options[0];
+
+    $scope.filterSelected = function(value) {
+        $scope.filter.push(value);
+    }
+
+    $scope.history = [];
+    $scope.isShowUsername = true;
+    $scope.isShowTimestamp = true;
+    $scope.isShowPrCh = true;
+    $scope.hideUsername = function () {
+        $scope.isShowUsername = $scope.isShowUsername ? false : true;
+    }
+    $scope.hideTimestamp = function () {
+        $scope.isShowTimestamp = $scope.isShowTimestamp ? false : true;
+    }
+    $scope.hidePrCh = function () {
+        $scope.isShowPrCh = $scope.isShowPrCh ? false : true;
+    }
+
+   
+    
+
+    // Releases.getReleases().success(function(response) {
+    //     console.log(response.releases,"respon");
+    //     // $scope.release_response = response.releases;
+    //     // Releases.getHistory($scope.release_response)
+    //     // .success(function(response) {
+    //     // });
+    // });
+
+    
+
 
 
 
