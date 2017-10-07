11071afa0bda0b1b410663659c2e3bdcfe42eb33
dmkitui <daniel.kitui@andela.com>
2017-10-07 13:06:36 +0300

    Refactor message for NaN validation errors

--- ui/app/js/services/helper_service.js
+++ ui/app/js/services/helper_service.js
@@ -15,7 +15,7 @@ angular.module("app").factory('Helpers', function() {
       for (var key in validation_fields) {
         var value = validation_fields[key];
         if (isNaN(value)) {
-          validation_results[key] = 'Invalid input for '+ key +'. Not an integer. '+ key +': "'+ value + '" is not a "'+ key + '"';
+          validation_results[key] = key + ' field value must be an integer. "' + value + '" is not an integer.';
           continue;
         }
         // Filter negative numbers and maximum, if specified.
