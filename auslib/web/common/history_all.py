73a51d91d99804ab43bf988192d668b5c9af4625
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-09 21:04:51 +0100

    create endpoint for permissions histories

--- auslib/web/common/history_all.py
+++ auslib/web/common/history_all.py
@@ -8,7 +8,7 @@ from auslib.web.common.history import HistoryHelper
 from sqlalchemy.sql.expression import null
 from auslib.web.common.rules import get_rules
 from auslib.web.common.releases import get_releases, process_release_revisions
-from auslib.web.admin.views.permissions import PermissionScheduledChangeHistoryView
+from auslib.web.admin.views.permissions import UsersView, PermissionScheduledChangeHistoryView
 
 
 log = logging.getLogger(__name__)
@@ -41,6 +41,12 @@ def get_rules_histories():
 def get_releases_histories():
     history_table = dbo.releases.history
     return _get_histories(history_table, get_releases, process_release_revisions)
+
+def get_permissions_histories():
+    history_table = dbo.permissions.history
+    get_permissions = UsersView().get()
+    return _get_histories(history_table, get_permissions)
+
 
 def get_scheduled_change_permissions_histories():
     """GET /history/scheduled_changes/permissions"""
