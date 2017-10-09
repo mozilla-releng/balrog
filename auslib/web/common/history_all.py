79ca67411dc0e9c349eaa9bda6426001fc324de5
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-09 16:21:54 +0100

    restructure histories GET method to be modular

--- auslib/web/common/history_all.py
+++ auslib/web/common/history_all.py
@@ -16,32 +16,28 @@ def _get_filters(obj, history_table):
     if history_table and obj:
         return [True, True]
 
-
-def get_rules_histories():
-    history_table = dbo.rules.history
+def _get_histories(table, obj, process_revisions_callback=None):
+    history_table = table
     order_by = [history_table.timestamp.desc()]
     history_helper = HistoryHelper(hist_table=history_table,
                                    order_by=order_by,
                                    get_object_callback=lambda: get_rules,
                                    history_filters_callback=_get_filters,
-                                   obj_not_found_msg='No history found for rules')
+                                   obj_not_found_msg='No history found',
+                                   process_revisions_callback=process_revisions_callback)
     try:
         return history_helper.get_history()
     except (ValueError, AssertionError) as msg:
         log.warning("Bad input: %s", msg)
-        return problem(400, "Bad Request", "Error occurred when trying to fetch rules history",
+        return problem(400, "Bad Request", "Error occurred when trying to fetch histories",
                        ext={"exception": str(msg)})
+
+def get_rules_histories():
+    history_table = dbo.rules.history
+    return _get_histories(history_table, get_rules)
+
 
 def get_releases_histories():
     history_table = dbo.releases.history
-    order_by = [history_table.timestamp.desc()]
-    history_helper = HistoryHelper(hist_table=history_table,
-                                   order_by=order_by,
-                                   get_object_callback=lambda: get_releases,
-                                   history_filters_callback=_get_filters,
-                                   process_revisions_callback=process_release_revisions)
-    try:
-        return history_helper.get_history()
-    except (ValueError, AssertionError) as e:
-        log.warning("Bad input: %s", json.dumps(e.args))
-        return problem(400, "Bad Request", "Invalid input", ext={"data": e.args})
+    return _get_histories(history_table, get_releases, process_release_revisions)
+
