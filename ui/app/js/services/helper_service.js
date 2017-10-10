39c9bec6385272eef7c02a17bd0e19fd9d3c510c
dmkitui <daniel.kitui@andela.com>
2017-10-10 19:58:23 +0300

    Add helper method to validate the inputs, and provide appropirate validation error messages

--- ui/app/js/services/helper_service.js
+++ ui/app/js/services/helper_service.js
@@ -14,6 +14,25 @@ angular.module("app").factory('Helpers', function() {
       $scope.pageSize = $scope.page_size.id;
       localStorage.setItem(controller_name, JSON.stringify($scope.page_size));
     },
+    integerValidator: function (validation_fields) {
+      var validation_results = {};
+      for (var key in validation_fields) {
+        var value = validation_fields[key];
+        if (isNaN(value)) {
+          validation_results[key] = key + ' field value must be an integer. "' + value + '" is not an integer.';
+          continue;
+        }
+        // Filter negative numbers and maximum, if specified.
+        if (value < 0) {
+          validation_results[key] = key + ' field value should be an integer >= 0 '+ key +': '+ value +' is not a "'+ key +'"';
+        } else if (key === 'backgroundRate' && value > 100) {
+          validation_results[key] = 'backgroundRate field value should be an integer <= 100 backgroundRate: '+ value + ' is not a "backgroundRate"';
+        } else {
+          validation_results[key] = false;
+        }
+      }
+      return validation_results;
+    }
   };
   return service;
 });
