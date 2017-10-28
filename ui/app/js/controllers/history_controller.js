ed5938488af07f9d98c97e1eeb6fb78e3387f5c1
Hope Ngerebara <hope.ngerebara@andela.com>
2017-10-28 08:27:41 +0100

    Adjustments to accomodate consmuing only one endpoint

--- ui/app/js/controllers/history_controller.js
+++ ui/app/js/controllers/history_controller.js
@@ -10,119 +10,27 @@ angular
       dateRangeEnd: "",
       hs_pr_ch_filter: ""
     };
-    $scope.isShowChangedBy = true;
-    $scope.isShowDaterange = true;
-    $scope.isShowPrCh = true;
     $scope.allHistory = [];
     $scope.pr_ch_options = [];
-    $scope.tableResult1 = false;
-    $scope.tableResult2 = false;
-    $scope.tableResult3 = false;
+    $scope.tableResult = false;
     $scope.calendar_is_open = false;
     $scope.loading = false;
     $scope.failed = false;
-    $scope.tab = 1;
-    $scope.rrp_filter = true;
-    $scope.search = [];
+    $scope.searchResult = [];
+    $scope.checkBoxes = {};
     
     $scope.currentPage = 1;
-    $scope.pageSize = 20;
+    $scope.pageSize = 100;
     $scope.maxSize = 10;
 
-    // Setting tabs
-    $scope.setTab = function(newTab) {
-      $scope.tab = newTab;
-      if ($scope.tab === 1) {
-        $scope.rrp_filter = true;
-        $scope.sc_filter = false;
-        $scope.signoff_filter = false;
-      } else if ($scope.tab === 2){
-        $scope.sc_filter = true;
-        $scope.signoff = false;
-        $scope.rrp_filter = false;
-      } else {
-        $scope.signoff_filter = true;
-        $scope.rrp_filter = false;
-        $scope.sc_filter = false;
-      }
+    $scope.checkbox_constants  = {
+      rules: 0,
+      releases:0,
+      permissions: 0,
+      permissions_required_signoffs: 0,
+      product_required_signoffs:0
     };
-
-    $scope.isSet = function(tabNum) {
-      return $scope.tab === tabNum;
-    };
-
-    // Checkboxes start
-    $scope.checkBoxes = [
-      {
-        id: 1,
-        name: "rules",
-        value: "Rules",
-        selected: false
-      },
-      {
-        id: 2,
-        name: "releases",
-        value: "Releases",
-        selected: false
-      },
-      {
-        id: 3,
-        name: "permissions",
-        value: "Permissions",
-        selected: false
-      }
-    ];
-
-    $scope.sc_checkBoxes = [
-      {
-        id: 1,
-        name: "rules_scheduled_change",
-        value: "Rules",
-        selected: false
-      },
-      {
-        id: 2,
-        name: "releases_scheduled_change",
-        value: "Releases",
-        selected: false
-      },
-      {
-        id: 3,
-        name: "permissions_scheduled_change",
-        value: "Permissions",
-        selected: false
-      },
-      {
-        id: 4,
-        name: "product_required_signoff_scheduled_change",
-        value: "Product Signoffs",
-        selected: false
-      },
-      {
-        id: 5,
-        name: "permissions_required_signoff_scheduled_change",
-        value: "Permissions Signoffs",
-        selected: false
-      }
-    ];
-
-
-    $scope.signoff_checkBoxes = [
-      {
-        id: 1,
-        name: "product_required_signoffs",
-        value: "Product Signoffs",
-        selected: false
-      },
-      {
-        id: 2,
-        name: "permissions_required_signoffs",
-        value: "Permissions Signoffs",
-        selected: false
-      }
-    ];
       
-
     //Add and remove username or email tags
     $scope.addChangedBy = function() {
       $scope.userInput.changedBy.push({ name: $scope.changedBy });
@@ -217,65 +125,53 @@ angular
     };
     //Product/channel filter ends
 
-    //To add and remove filter fields
-    $scope.getOption = function() {
-      var selected = $scope.selected;
-      switch (selected) {
-        case "changedBy":
-          $scope.isShowChangedBy = true;
-          break;
-        case "daterange":
-          $scope.isShowDaterange = true;
-          break;
-        case "product_channel":
-          $scope.hs_pr_ch_filter = "";
-          $scope.isShowPrCh = true;
-          break;
-      }
-      $scope.selected = "";
-    };
-
-    $scope.hideChangedBy = function() {
-      $scope.isShowChangedBy = false;
-      $scope.userInput.changedBy = "";
-    };
-    $scope.hideDaterange = function() {
-      $scope.isShowDaterange = false;
-      $scope.userInput.dateRangeStart = "";
-      $scope.userInput.dateRangeEnd = "";
-    };
-    $scope.hidePrCh = function() {
-      $scope.isShowPrCh = false;
-    };
-    //End add and remove filter fields
     
-
+    function optionChecked(choice) {
+      angular.forEach(choice, function(value, key) {
+        if (value === true) {
+          $scope.checkbox_constants[key]= 1; 
+        }else{
+          $scope.checkbox_constants[key] = 0;
+        }
+      });
+      return $scope.checkbox_constants;
+    }
+    
     function checkParameters() {
-      if ($scope.userInput.changedBy !== "") {
-        var changedByArr = [];
-        $scope.userInput.changedBy.forEach(function(item) {
-          if (changedByArr.indexOf(item.name) === -1) {
-            changedByArr.push(item.name);
+      $scope.checkboxValues = optionChecked($scope.checkBoxes);
+      angular.forEach($scope.checkboxValues, function(value, key){
+        if (value === 1) {
+          if ($scope.userInput.changedBy) {
+            var changedByArr = [];
+            $scope.userInput.changedBy.forEach(function(item) {
+              if (changedByArr.indexOf(item.name) === -1) {
+                changedByArr.push(item.name);
+              }
+              $scope.changedByValue = changedByArr;
+            });
           }
-          $scope.changedByValue = changedByArr;
-        });
-      }
-      if ($scope.userInput.dateRangeStart ||$scope.userInput.dateRangeEnd) { 
-        $scope.hs_startDate = new Date($scope.userInput.dateRangeStart).getTime();
-        $scope.hs_endDate = new Date($scope.userInput.dateRangeEnd).getTime();
-      }
-      if ($scope.userInput.hs_pr_ch_filter && $scope.pr_ch_selected !== undefined) {
-        $scope.product = $scope.pr_ch_selected[0];
-        $scope.channel = $scope.pr_ch_selected[1];   
-      } else {
-        return false;
-      }
+          if ($scope.userInput.dateRangeStart && $scope.userInput.dateRangeEnd) { 
+            $scope.hs_startDate = new Date($scope.userInput.dateRangeStart).getTime();
+            $scope.hs_endDate = new Date($scope.userInput.dateRangeEnd).getTime();
+          }
+          if ($scope.userInput.hs_pr_ch_filter && $scope.pr_ch_selected !== undefined) {
+            $scope.product = $scope.pr_ch_selected[0];
+            $scope.channel = $scope.pr_ch_selected[1];   
+          } 
+        } else {
+          // sweetAlert(
+          //   "Please check a box",
+          //   "You cannot proceed",
+          //   "error"
+          // );
+        }
+      })
     }
 
     //Request response
-    var result = [];
     function processResponse(response) {
-      $scope.history_count = 0 ;
+      var result = [];
+      $scope.history_count = 0;
       angular.forEach(response, function(value, key){
         $scope.changeType = key;
         $scope.history_revisions = value.revisions;
@@ -283,9 +179,7 @@ angular
         if ($scope.history_count > 0){
           $scope.history_revisions.forEach(function(revision){
             result.push(revision);
-              $scope.search = result;
-              return $scope.search;
-            });
+          });
         }else {
             sweetAlert(
               "error",
@@ -293,132 +187,46 @@ angular
               "error"   
             );
           }
-      });    
-       
+      });         
+      $scope.searchResult = result;
+      
     }
     //Request response ends
 
-    function rrpHistory () {    
-      checkParameters();
-      var filterParams = {
-        changedByValue: $scope.changedByValue,
-        startDate: $scope.hs_startDate,
-        endDate: $scope.hs_endDate,
-        product: $scope.product,
-        channel: $scope.channel
-      };
-      History.getrrpHistory($scope.checkboxValues, filterParams)
-      .success(function(response) {
-        if(Object.keys(response).length > 0){
-          $scope.tableResult1 = true;
-          processResponse(response); 
-        } else {
-          $scope.search = [];
-          $scope.tableResult1 = false;
-          return $scope.search;
-        }   
-      })
-      .error(function() {
-        console.log("error");
-      });
-    }
-
-    function scHistory() {
-      checkParameters();
-      var filterParams = {
-        changedByValue: $scope.changedByValue,
-        startDate: $scope.hs_startDate,
-        endDate: $scope.hs_endDate,
-        product: $scope.product,
-        channel: $scope.channel
-      };
-      History.getscHistory($scope.checkboxValues, filterParams)
-      .success(function(response) {
-        if(Object.keys(response).length > 0){
-          $scope.tableResult2 = true;
-          processResponse(response); 
-        } else {
-          $scope.search = [];
-          $scope.tableResult2 = false;
-          return $scope.search;
-        }   
-      })
-      .error(function() {
-        console.log("error");
-      });   
-    }
-
-    function signoffHistory() {
-      checkParameters();
-      var filterParams = {
-        changedByValue: $scope.changedByValue,
-        startDate: $scope.hs_startDate,
-        endDate: $scope.hs_endDate,
-        product: $scope.product,
-        channel: $scope.channel
-      };
-      History.getsignoffHistory($scope.checkboxValues, filterParams)
-      .success(function(response) {
-        if(Object.keys(response).length > 0){
-          $scope.tableResult3 = true;
-          processResponse(response); 
-        } else {
-          $scope.search = [];
-          $scope.tableResult3 = false;
-          return $scope.search;
-        }   
-      })
-      .error(function() {
-        console.log("error");
-      });   
-    }
-
-    function optionChecked(choice) {
-      angular.forEach(choice, function(value, key) {
-        if (choice[key].selected) {
-          $scope.constants[choice[key].name] = 1;
-        }else{
-          $scope.constants[choice[key].name] = 0;
-        }
-      });
-      return $scope.constants;
-    }
 
     $scope.searchHistory = function() {
-      
-      switch ($scope.tab) {
-        case 1:
-          var rrp_constants = {rules: 0, releases:0, permissions:0 };
-          $scope.constants = rrp_constants;
-          $scope.checkboxValues = optionChecked($scope.checkBoxes);
-          rrpHistory(); 
-          break;
-        case 2:
-          var sc_constants = {
-            rules_scheduled_change: 0,
-            releases_scheduled_change:0,
-            permissions_scheduled_change:0,
-            permissions_required_signoff_scheduled_change:0,
-            product_required_signoff_scheduled_change:0
-          };
-          $scope.constants = sc_constants;
-          $scope.checkboxValues = optionChecked($scope.sc_checkBoxes);
-          scHistory(); 
-          break;
-        case 3:
-          var signoff_constants = {
-            permissions_required_signoffs: 0,
-            product_required_signoffs:0
-          };
-          $scope.constants = signoff_constants;
-          $scope.checkboxValues = optionChecked($scope.signoff_checkBoxes);
-          signoffHistory();
-          break;
-      }
+      $scope.loading = true;
+      checkParameters();
+      var filterParams = {
+        checkboxValue: $scope.checkboxValues,
+        changedByValue: $scope.changedByValue,
+        startDate: $scope.hs_startDate,
+        endDate: $scope.hs_endDate,
+        product: $scope.product,
+        channel: $scope.channel
+      };
+      History.getHistory(filterParams)
+      .success(function(response) {
+        if(Object.keys(response).length > 0){
+          $scope.tableResult = true;
+          processResponse(response); 
+        } else {
+          $scope.searchResult = [];
+          $scope.tableResult = false;
+          return $scope.searchResult;
+        }
+      })
+      .error(function() {
+        console.error(arguments);
+        $scope.failed = true;
+      })
+      .finally(function() {
+        $scope.loading = false;
+      });
     };
 
     $scope.openDataModal = function(change_id) {
-      angular.forEach($scope.search, function(revision){
+      angular.forEach($scope.searchResult, function(revision){
         if(revision.change_id === change_id ){
           var modalInstance = $modal.open({
             templateUrl: 'history_data_modal.html',
@@ -437,9 +245,6 @@ angular
             }
           });
         } 
-      });
-          
+      });       
     };
-    
-  
   });
--- ui/app/js/services/history_service.js
+++ ui/app/js/services/history_service.js
@@ -1,60 +1,28 @@
 angular.module("app").factory('History', function($http, $q, ScheduledChanges, Helpers) {
     var service = {
-      getrrpHistory: function(checkboxValues, filterParams){
-        var url = '/api/rrp/history';
-        url += '?rules=' +checkboxValues.rules + '&releases=' +checkboxValues.releases + '&permissions=' +checkboxValues.permissions;
+      getHistory: function(filterParams){
+        var url = '/api/history?releases=1';
+        if (filterParams.checkboxValue){
+          angular.forEach(filterParams.checkboxValue, function(value,key) {
+            if (value === 1) {
+              url += `&${key}=`+ value;
+            }
+          })
+        }
         if (filterParams.changedByValue){
           url += '&changed_by=' + filterParams.changedByValue;
         }
-        if (filterParams.startDate && filterParams.endDate){
-          url += '&timestamp_from=' + filterParams.startDate + '&timestamp_to=' + filterParams.endDate;
-        }
+        if (filterParams.startDate || filterParams.endDate){
+            url += '&timestamp_from=' + filterParams.startDate+ '&timestamp_to=' + filterParams.endDate;
+          }
         if (filterParams.product && filterParams.channel){
           url += '&product=' + filterParams.product + '&channel=' + filterParams.channel;
         } else if (filterParams.product){
           url += '&product=' + filterParams.product;
-        }
+        }     
         return $http.get(url) ;
       },
-
-      getscHistory: function(checkboxValues, filterParams) {
-        var url = '/api/sc/history';
-        url += '?rules_scheduled_change=' +checkboxValues.rules_scheduled_change +
-        '&releases_scheduled_change=' +checkboxValues.releases_scheduled_change +
-        '&permissions_scheduled_change=' +checkboxValues.permissions_scheduled_change +
-        '&permissions_required_signoff_scheduled_change=' +checkboxValues.permissions_required_signoff_scheduled_change +
-        '&product_required_signoff_scheduled_change=' +checkboxValues.product_required_signoff_scheduled_change;
-        if (filterParams.changedByValue){
-          url += '&changed_by=' + filterParams.changedByValue;
-        }
-        if (filterParams.startDate && filterParams.endDate){
-          url += '&timestamp_from=' + filterParams.startDate + '&timestamp_to=' + filterParams.endDate;
-        }
-        if (filterParams.product && filterParams.channel){
-          url += '&product=' + filterParams.product + '&channel=' + filterParams.channel;
-        } else if (filterParams.product){
-          url += '&product=' + filterParams.product;
-        }
-        return $http.get(url) ;
-      },
-      getsignoffHistory: function(checkboxValues, filterParams) {
-        var url = '/api/required_signoff/history';
-        url += '?permissions_required_signoffs=' +checkboxValues.permissions_required_signoffs +
-        '&product_required_signoffs=' +checkboxValues.product_required_signoffs;
-        if (filterParams.changedByValue){
-          url += '&changed_by=' + filterParams.changedByValue;
-        }
-        if (filterParams.startDate && filterParams.endDate){
-          url += '&timestamp_from=' + filterParams.startDate + '&timestamp_to=' + filterParams.endDate;
-        }
-        if (filterParams.product && filterParams.channel){
-          url += '&product=' + filterParams.product + '&channel=' + filterParams.channel;
-        } else if (filterParams.product){
-          url += '&product=' + filterParams.product;
-        }
-        return $http.get(url) ;
-      },    
-    };
+    }
     return service; 
   });
   
