453f5751f07341ebf80017c0d2c00f07e6c43278
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-11-08 14:16:27 +0100

    return deleted method

--- auslib/web/common/history.py
+++ auslib/web/common/history.py
@@ -53,6 +53,20 @@ class HistoryHelper():
         return jsonify(ret)
 
 
+def get_input_dict():
+    reserved_filter_params = ['limit', 'page', 'timestamp_from', 'timestamp_to']
+    args = request.args
+    query_keys = []
+    query = {}
+    for key in args:
+        if key not in reserved_filter_params:
+            query_keys.append(key)
+
+    for key in query_keys:
+        query[key] = request.args.get(key)
+    return jsonify(query_keys=query_keys, query=query)
+
+
 history_keys = ('timestamp', 'change_id', 'data_version', 'changed_by')
 
 
