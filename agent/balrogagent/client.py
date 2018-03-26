8e11177af571719f1d09dbaa0b3bfd02793d49e7
Ben Hearsum <bhearsum@mozilla.com>
2018-03-26 16:33:02 -0400

    Kindof fix stuff?

--- agent/balrogagent/client.py
+++ agent/balrogagent/client.py
@@ -23,13 +23,12 @@ async def request(api_root, path, method="GET", data={}, headers=default_headers
     # CSRF tokens are only required for POST/PUT/DELETE.
     if method not in ("HEAD", "GET"):
         logging.debug("Sending %s request to %s", "HEAD", csrf_url)
-        resp = await aiohttp.request("HEAD", csrf_url, auth=auth, loop=loop)
-        resp.raise_for_status()
-        data["csrf_token"] = resp.headers["X-CSRF-Token"]
-        resp.close()
+        async with aiohttp.request("HEAD", csrf_url, auth=auth, loop=loop) as resp:
+            resp.raise_for_status()
+            data["csrf_token"] = resp.headers["X-CSRF-Token"]
 
     logging.debug("Sending %s request to %s", method, url)
-    resp = await aiohttp.request(method, url, data=json.dumps(data), headers=headers, auth=auth, loop=loop)
-    # Raises on 400 code or higher, we can assume things are good if we make it past this.
-    resp.raise_for_status()
-    return resp
+    async with aiohttp.request(method, url, data=json.dumps(data), headers=headers, auth=auth, loop=loop) as resp:
+        # Raises on 400 code or higher, we can assume things are good if we make it past this.
+        resp.raise_for_status()
+        return resp
