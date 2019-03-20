ba7a185dc93d085dc45bd911d2ee7c8cf4bc2677
Ben Hearsum <bhearsum@mozilla.com>
2019-03-20 10:12:22 -0400

    Hide nav links until user logs in.

--- ui/app/js/controllers/dashboard_controller.js
+++ ui/app/js/controllers/dashboard_controller.js
@@ -1,9 +1,10 @@
 angular.module("app").controller('DashboardController',
-function($scope, $location, cssInjector, $localForage, Page) {
+function($scope, $location, cssInjector, $localForage, Page, Auth0) {
   cssInjector.add('//fonts.googleapis.com/css?family=Bangers');
 
   Page.setTitle('Home');
 
+  $scope.auth0 = Auth0;
   $scope.saved_searches = [];
   $localForage.getItem('savedSearches')
   .then(function(data) {
