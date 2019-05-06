ef8b440b0e4ae09bfce310b011ea9f127b0706fe
Ben Hearsum <bhearsum@mozilla.com>
2019-05-06 16:18:40 -0400

    Formatting pass.

--- scripts/releases-history-to-gcs.py
+++ scripts/releases-history-to-gcs.py
@@ -12,7 +12,7 @@ import aiohttp
 from gcloud.aio.storage import Storage
 
 
-def ignore_aiohttp_ssl_error(loop, aiohttpversion='3.5.4'):
+def ignore_aiohttp_ssl_error(loop, aiohttpversion="3.5.4"):
     """Ignore aiohttp #3535 issue with SSL data after close
 
     There appears to be an issue on Python 3.7 and aiohttp SSL that throws a
@@ -35,21 +35,23 @@ def ignore_aiohttp_ssl_error(loop, aiohttpversion='3.5.4'):
     orig_handler = loop.get_exception_handler() or loop.default_exception_handler
 
     def ignore_ssl_error(loop, context):
-        if context.get('message') == 'SSL error in data received':
+        if context.get("message") == "SSL error in data received":
             # validate we have the right exception, transport and protocol
-            exception = context.get('exception')
-            protocol = context.get('protocol')
+            exception = context.get("exception")
+            protocol = context.get("protocol")
             if (
-                isinstance(exception, ssl.SSLError) and exception.reason == 'KRB5_S_INIT' and
-                isinstance(protocol, asyncio.sslproto.SSLProtocol) and
-                isinstance(protocol._app_protocol, aiohttp.client_proto.ResponseHandler)
+                isinstance(exception, ssl.SSLError)
+                and exception.reason == "KRB5_S_INIT"
+                and isinstance(protocol, asyncio.sslproto.SSLProtocol)
+                and isinstance(protocol._app_protocol, aiohttp.client_proto.ResponseHandler)
             ):
                 if loop.get_debug():
-                    asyncio.log.logger.debug('Ignoring aiohttp SSL KRB5_S_INIT error')
+                    asyncio.log.logger.debug("Ignoring aiohttp SSL KRB5_S_INIT error")
                 return
         orig_handler(context)
 
     loop.set_exception_handler(ignore_ssl_error)
+
 
 skip_patterns = ("-nightly-",)
 
@@ -120,7 +122,6 @@ async def main(loop, balrog_api, bucket_name, limit_to, concurrency):
                 uploads[u]["uploaded"] += processed_uploads[u]["uploaded"]
                 uploads[u]["existing"] += processed_uploads[u]["existing"]
 
-
     for r in releases:
         revs_in_gcs = uploads[r]["uploaded"] + uploads[r]["existing"]
         print("INFO: {}: Found {} existing revisions, uploaded {} new ones".format(r, uploads[r]["existing"], uploads[r]["uploaded"]))
@@ -128,6 +129,7 @@ async def main(loop, balrog_api, bucket_name, limit_to, concurrency):
             print("WARNING: {} was found in the Balrog API but does not exist in GCS".format(r))
         elif releases[r] != revs_in_gcs:
             print("WARNING: {} has a data version of {} in the Balrog API, but {} revisions exist in GCS".format(r, releases[r], revs_in_gcs))
+
 
 balrog_api = sys.argv[1]
 bucket_name = sys.argv[2]
