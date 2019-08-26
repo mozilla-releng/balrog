2976f6b2810ec2239ec452ac53a21d92c43df53f
allan.silva <allan.tavares@allantavares.com.br>
2019-08-26 10:39:12 -0300

    Fix display number of signoffs in release SC page

--- ui/app/js/controllers/release_scheduled_changes_controller.js
+++ ui/app/js/controllers/release_scheduled_changes_controller.js
@@ -92,7 +92,7 @@ function($scope, $routeParams, $location, $timeout, Search, $modal, $route, Rele
         sc.when = new Date(sc.when);
       }
 
-      if (!sc.complete && $scope.isScheduledToBeModifiable(sc)) {
+      if (!sc.complete && $scope.isScheduledToBeModifiable(sc) && Object.keys(sc.required_signoffs).length === 0) {
         Releases.getRequiredSignoffsForProduct(sc.name).success(function(response) {
           sc.required_signoffs = response.required_signoffs;
         });
