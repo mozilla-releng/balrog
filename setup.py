7924732fab759b44e67ede677e83b3443b971473
Ben Hearsum <bhearsum@mozilla.com>
2019-09-13 14:43:10 -0400

    Fix missing dependencies that were breaking tests.

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ here = path.abspath(path.dirname(__file__))
 # of the packages in it. However, setuptools doesn't support parsing this type
 # of file, so we need to strip those out before passing the requirements along
 # to it.
-with open(path.join(here, "requirements/base.txt")) as f:
+with open(path.join(here, "requirements", "base.txt")) as f:
     requirements = []
     for line in f:
+        # Skip empty lines
+        if not line.strip():
+            continue
         # Skip lines with hash values
         if not line.strip().startswith("--"):
             requirements.append(line.split(";")[0].split()[0])
