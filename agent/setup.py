e67f3b75092c8cc6bd2f9377091a924c4db1568b
Ben Hearsum <bhearsum@mozilla.com>
2019-09-13 17:16:12 -0400

    Update agent requirements files.

--- agent/setup.py
+++ agent/setup.py
@@ -9,12 +9,15 @@ here = path.abspath(path.dirname(__file__))
 # of the packages in it. However, setuptools doesn't support parsing this type
 # of file, so we need to strip those out before passing the requirements along
 # to it.
-with open(path.join(here, "requirements.txt")) as f:
+with open(path.join(here, "requirements", "base.txt")) as f:
     requirements = []
     for line in f:
+        # Skip empty lines
+        if not line.strip():
+            continue
         # Skip lines with hash values
         if not line.strip().startswith("--"):
-            requirements.append(line.split()[0])
+            requirements.append(line.split(";")[0].split()[0])
 
 with open(path.join(here, "version.txt")) as f:
     version = f.read()
