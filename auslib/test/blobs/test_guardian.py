fc10375b5700f65077133dca0dc0cc304250c8b0
Ben Hearsum <bhearsum@mozilla.com>
2019-10-03 11:58:50 -0400

    Minor naming/comment changes.

--- auslib/test/blobs/test_guardian.py
+++ auslib/test/blobs/test_guardian.py
@@ -3,8 +3,8 @@ import pytest
 from auslib.blobs.guardian import GuardianBlob
 
 
-@pytest.fixture
-def guardblob():
+@pytest.fixture(scope="session")
+def guardianblob():
     blob = GuardianBlob()
     blob.loadJSON(
         """
@@ -29,21 +29,21 @@ def guardblob():
 
 
 @pytest.mark.parametrize("whitelistedDomains,expected", [({"a.com": ("Guardian",)}, False), ({}, True)])
-def testContainsForbiddenDomain(guardblob, whitelistedDomains, expected):
-    assert guardblob.containsForbiddenDomain("Guardian", whitelistedDomains) is expected
+def testContainsForbiddenDomain(guardianblob, whitelistedDomains, expected):
+    assert guardianblob.containsForbiddenDomain("Guardian", whitelistedDomains) is expected
 
 
 @pytest.mark.parametrize(
     "version,expected", [("0.5.0.0", True), ("0.8.0.0", True), ("0.99.99.99", True), ("1.0.0.0", False), ("1.0.5.0", False), ("2.0.0.0", False)]
 )
-def testShouldServeUpdateVariousVersions(guardblob, version, expected):
+def testShouldServeUpdateVariousVersions(guardianblob, version, expected):
     updateQuery = {"product": "Guardian", "version": version, "buildTarget": "WINNT_x86_64", "channel": "release"}
-    assert guardblob.shouldServeUpdate(updateQuery) is expected
+    assert guardianblob.shouldServeUpdate(updateQuery) is expected
 
 
-def testShouldServeUpdateMissingBuildTarget(guardblob):
+def testShouldServeUpdateMissingBuildTarget(guardianblob):
     updateQuery = {"product": "Guardian", "version": "0.5.0.0", "buildTarget": "Linux_x86_64", "channel": "release"}
-    assert not guardblob.shouldServeUpdate(updateQuery)
+    assert not guardianblob.shouldServeUpdate(updateQuery)
 
 
 @pytest.mark.parametrize(
@@ -55,6 +55,6 @@ def testShouldServeUpdateMissingBuildTarget(guardblob):
         ("WINNT_x86_64", {}, {}),
     ],
 )
-def testGetResponse(guardblob, buildTarget, whitelistedDomains, expected):
+def testGetResponse(guardianblob, buildTarget, whitelistedDomains, expected):
     updateQuery = {"product": "Guardian", "version": "0.5.0.0", "buildTarget": buildTarget, "channel": "release"}
-    assert guardblob.getResponse(updateQuery, whitelistedDomains) == expected
+    assert guardianblob.getResponse(updateQuery, whitelistedDomains) == expected
