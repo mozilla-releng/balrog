c6d0338fcaf67b7d0885462f043246e086c9aa75
Ben Hearsum <bhearsum@mozilla.com>
2020-01-07 12:44:53 -0500

    Delete old UI

--- ui/app/js/services/return_title.js
+++
@@ -1,7 +0,0 @@
-angular.module("app").factory('Page', function() {
-   var title = '';
-   return {
-     title: function() { return title + ' - Balrog Admin'; },
-     setTitle: function(newTitle) { title = newTitle; }
-   };
-});
