f34ea7725a59eb6bba5262426b04ea5838abcb60
Ben Hearsum <bhearsum@mozilla.com>
2020-02-11 13:22:22 -0500

    Implement /v2/releases/{name}/data_versions GET

--- tests/admin/views/test_releases_v2.py
+++ tests/admin/views/test_releases_v2.py
@@ -510,6 +510,26 @@ def test_get_release_scheduled_delete(api, firefox_67_0_build1):
     assert ret.json == expected, ret.json
 
 
+@pytest.mark.usefixtures("releases_db")
+def test_get_data_versions_404(api):
+    ret = api.get("/v2/releases/Firefox-33.0-build1/data_versions")
+    assert ret.status_code == 404, ret.data
+
+
+@pytest.mark.usefixtures("releases_db")
+def test_get_data_versions(api, firefox_56_0_build1):
+    ret = api.get("/v2/releases/Firefox-56.0-build1/data_versions")
+    assert ret.status_code == 200, ret.data
+
+    expected = {"data_versions": infinite_defaultdict()}
+    expected["data_versions"]["."] = 1
+    for pname, pdata in firefox_56_0_build1["platforms"].items():
+        for lname in pdata.get("locales", {}):
+            expected["data_versions"]["platforms"][pname]["locales"][lname] = 1
+
+    assert ret.json == expected, ret.json
+
+
 @pytest.mark.usefixtures("releases_db", "mock_verified_userinfo")
 def test_put_fails_when_signoff_required(api, firefox_56_0_build1):
     firefox_56_0_build1 = deepcopy(firefox_56_0_build1)
