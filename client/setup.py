54b01d2058b7a7ba447d76a48c64958e5ed3f6d8
Ben Hearsum <bhearsum@mozilla.com>
2020-02-20 15:21:57 -0500

    Allow longer lines before eliding (#1197)
    * Allow longer lines before eliding
    
    * Version bump
    
    * Make max line length a constant

--- client/setup.py
+++ client/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="balrogclient",
-    version="1.1.0",
+    version="1.1.1",
     description="Balrog Admin API Client",
     author="Mozilla Release Engineers",
     author_email="release+python@mozilla.com",
