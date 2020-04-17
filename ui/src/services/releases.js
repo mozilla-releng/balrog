41578b4d6bdd52ffcef40b0d50e87b97d41866a1
Ben Hearsum <bhearsum@mozilla.com>
2020-04-17 10:46:31 -0400

    Add new helpers for querying V2 releases api

--- ui/src/services/releases.js
+++ ui/src/services/releases.js
@@ -1,60 +1,167 @@
 import axios from 'axios';
 import { stringify } from 'qs';
 
+const getReleasesV2 = () => axios.get('/v2/releases');
+const getReleaseV2 = name =>
+  axios.get(`/v2/releases/${encodeURIComponent(name)}`);
+const setRelease = ({ name, blob, oldDataVersions, when, product }) =>
+  axios.put(`/v2/releases/${encodeURIComponent(name)}`, {
+    name,
+    blob,
+    old_data_versions: oldDataVersions,
+    when,
+    product,
+  });
+const deleteReleaseV2 = name =>
+  axios.delete(`/v2/releases/${encodeURIComponent(name)}`);
+const setReadOnlyV2 = (name, readOnly, oldDataVersion) =>
+  axios.put(`/v2/releases/${encodeURIComponent(name)}/read_only`, {
+    read_only: readOnly,
+    old_data_version: oldDataVersion,
+  });
 const getReleases = () => axios.get('/releases');
 const getRelease = name => axios.get(`/releases/${encodeURIComponent(name)}`);
-const getReleaseNames = () => axios.get(`/releases?names_only=1`);
+const getReleaseNames = () => axios.get('/releases?names_only=1');
+const getReleaseNamesV2 = () =>
+  getReleasesV2().then(result => result.data.releases.map(r => r.name));
+const makeSignoffV2 = (name, role) =>
+  axios.put(`/v2/releases/${encodeURIComponent(name)}/signoff`, { role });
+const revokeSignoffV2 = name =>
+  axios.delete(`/v2/releases/${encodeURIComponent(name)}/signoff`);
 const deleteRelease = ({ name, dataVersion }) =>
-  axios.delete(`/releases/${name}`, { params: { data_version: dataVersion } });
+  axios.delete(`/releases/${encodeURIComponent(name)}`, {
+    params: { data_version: dataVersion },
+  });
 const setReadOnly = ({ name, readOnly, dataVersion }) =>
-  axios.put(`/releases/${name}/read_only`, {
+  axios.put(`/releases/${encodeURIComponent(name)}/read_only`, {
     read_only: readOnly,
     data_version: dataVersion,
   });
-const getRevisions = (name, product) => {
-  const releases = [];
+const getRevisions = (name, apiVersion) => {
   const bucket = name.includes('nightly')
     ? process.env.GCS_NIGHTLY_HISTORY_BUCKET
     : process.env.GCS_RELEASES_HISTORY_BUCKET;
 
-  function parseReleases(rawReleases) {
-    if (rawReleases) {
-      rawReleases.forEach(r => {
-        const parts = r.name
-          .replace(`${name}/`, '')
+  // Version 1 API Releases have one prefix per Release (${name}/)
+  if (apiVersion === 1) {
+    const releases = [];
+
+    // eslint-disable-next-line no-inner-declarations
+    function parseReleases(rawReleases) {
+      if (rawReleases) {
+        rawReleases.forEach(r => {
+          const parts = r.name
+            .replace(`${name}/`, '')
+            .replace('.json', '')
+            .split('-');
+          const dataVersion = parseInt(parts[0], 10);
+          const release = {
+            name,
+            path: null,
+            data_version: Number(dataVersion) ? dataVersion : parts[0],
+            timestamp: parseInt(parts[1], 10),
+            changed_by: parts.slice(2).join('-'),
+            data_url: r.mediaLink,
+          };
+
+          releases.push(release);
+        });
+      }
+    }
+
+    // eslint-disable-next-line no-inner-declarations
+    async function getReleases(url, pageToken) {
+      const response = await axios.get(
+        pageToken ? `${url}&pageToken=${pageToken}` : url
+      );
+
+      parseReleases(response.data.items);
+
+      if (response.data.nextPageToken) {
+        return getReleases(url, response.data.nextPageToken);
+      }
+
+      // descending sort, so newer versions appear first
+      return releases.sort((a, b) => a.data_version < b.data_version);
+    }
+
+    return getReleases(`${bucket}?prefix=${name}/&delimeter=/`);
+  }
+
+  // Version 2 API Releases have 1 prefix per Release (${name})
+  // but must be grouped by whatever the string prior to "/" is.
+  // The combination of the latest entry from each group adds
+  // up to be the "current" version of the Release, while the older
+  // revisions are outdated versions.
+
+  function parseRevisions(rawRevisions) {
+    const revisions = [];
+
+    if (rawRevisions) {
+      rawRevisions.forEach(r => {
+        if (!r.name.startsWith(`${name}/`) && !r.name.startsWith(`${name}-.`)) {
+          return;
+        }
+
+        const [path, rest] = r.name
+          .replace(`${name}-`, '')
+          .replace(`${name}`, '')
           .replace('.json', '')
-          .split('-');
+          .split('/');
+        const parts = rest.split('-');
         const dataVersion = parseInt(parts[0], 10);
-        const release = {
+        const revision = {
           name,
-          product,
+          path,
           data_version: Number(dataVersion) ? dataVersion : parts[0],
           timestamp: parseInt(parts[1], 10),
           changed_by: parts.slice(2).join('-'),
           data_url: r.mediaLink,
         };
 
-        releases.push(release);
+        revisions.push(revision);
       });
     }
+
+    return revisions;
   }
 
-  async function getReleases(url, pageToken) {
+  // make sure this works with page tokens
+  async function getRevisions(url, pageToken, revisionsByPath) {
     const response = await axios.get(
       pageToken ? `${url}&pageToken=${pageToken}` : url
     );
 
-    parseReleases(response.data.items);
+    parseRevisions(response.data.items).forEach(r => {
+      if (!(r.path in revisionsByPath)) {
+        // eslint-disable-next-line no-param-reassign
+        revisionsByPath[r.path] = [];
+      }
+
+      revisionsByPath[r.path].push(r);
+    });
 
     if (response.data.nextPageToken) {
-      return getReleases(url, response.data.nextPageToken);
+      return getRevisions(url, response.data.nextPageToken, revisionsByPath);
     }
 
-    // descending sort, so newer versions appear first
-    return releases.sort((a, b) => a.data_version < b.data_version);
+    const latestEntries = [];
+    const revisions = [];
+
+    Object.values(revisionsByPath).forEach(pathRevisions => {
+      pathRevisions.sort((a, b) => a.timestamp > b.timestamp);
+      // Split up the most recent revision for each path, and the
+      // the remaining revisions.
+      latestEntries.push(pathRevisions.pop());
+      revisions.push(...pathRevisions);
+    });
+
+    revisions.sort((a, b) => a.timestamp < b.timestamp);
+
+    return [latestEntries, revisions];
   }
 
-  return getReleases(`${bucket}?prefix=${name}/&delimeter=/`);
+  return getRevisions(`${bucket}?prefix=${name}&delimeter=/`, null, {});
 };
 
 const getScheduledChanges = all => {
@@ -81,13 +188,21 @@ const deleteScheduledChange = ({ scId, scDataVersion }) =>
     params: { data_version: scDataVersion },
   });
 const getRequiredSignoffsForProduct = name =>
-  axios.get(`/releases/${name}/read_only/product/required_signoffs`);
+  axios.get(
+    `/releases/${encodeURIComponent(name)}/read_only/product/required_signoffs`
+  );
 
 // Releases factory
 export {
+  getReleasesV2,
+  getReleaseV2,
+  setRelease,
+  deleteReleaseV2,
+  setReadOnlyV2,
   getReleases,
   getRelease,
   getReleaseNames,
+  getReleaseNamesV2,
   deleteRelease,
   setReadOnly,
   getRevisions,
@@ -99,4 +214,6 @@ export {
   updateScheduledChange,
   deleteScheduledChange,
   getRequiredSignoffsForProduct,
+  makeSignoffV2,
+  revokeSignoffV2,
 };
