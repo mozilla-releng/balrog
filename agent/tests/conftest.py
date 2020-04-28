44de59ef259e327f3a94269ab52d011ad8305c3a
Ben Hearsum <bhearsum@mozilla.com>
2020-04-28 15:27:00 -0400

    Add logging; check signoff requirements for v2 endpoints; add tests

---
+++ agent/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from asynctest import MagicMock
+
+
+@pytest.fixture
+def fake_request():
+    def fake_request_factory(return_values={}):
+        async def do_fake_request(api_root, path, auth0_secrets, method="GET", data={}, headers={}, loop=None):
+            if method == "GET":
+                endpoint = path[1:]
+                ret = return_values.get(endpoint, [])
+                if "v2" in endpoint:
+                    return ret
+                else:
+                    return {"count": len(ret), "scheduled_changes": ret}
+            else:
+                return ""
+
+        return MagicMock(side_effect=do_fake_request)
+
+    return fake_request_factory
