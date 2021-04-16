ae5d4e2ff85d87380b2486a71d021606b49c962e
Aki Sasaki <aki@mozilla.com>
2021-04-15 18:55:03 -0700

    Support globbed version matching.
    In SystemAddons, we want to target specific version ranges, e.g. `80.*`.
    Previous to this patch, that meant we needed both a `<80` rule to
    prevent sending the same System Addon(s) to previous versions, and a
    `<81` rule to target the `80.*` range. This worked while we maintained
    SystemAddons rules manually, but will make it difficult to rewrite rules
    automatically.
    
    This patch adds support for `version: 80.*` or `version: 80.1.*` type
    globs.

--- src/auslib/util/comparison.py
+++ src/auslib/util/comparison.py
@@ -14,7 +14,15 @@ def has_operator(value):
     return value.startswith(("<", ">"))
 
 
+def glob_op(value, operand):
+    """Compare a string or StrictVersion against a glob stripped of its `*`.
+    Currently only supported in version_compare."""
+    return value.startswith(operand)
+
+
 def get_op(pattern):
+    if pattern.endswith(".*"):
+        return glob_op, pattern.rstrip("*")
     # only alphanumeric characters means no operator
     if re.match(r"\w+", pattern):
         return operator.eq, pattern
@@ -50,6 +58,23 @@ def version_compare(value, compstr, versionClass=MozillaVersion):
     eg version_compare('1.1', '>1.0') is True
     """
     opfunc, operand = get_op(compstr)
-    value = versionClass(value)
-    operand = versionClass(operand)
+    if opfunc is not glob_op:
+        # `StrictVersion.__str__` drops the final digit if it's a 0. For example,
+        #     str(StrictVersion(80.0.0)) -> "80.0"
+        # This breaks if we want to match 80.0.0 (80.0) against `80.0.*`:
+        #     "80.0".startswith("80.0.") -> False
+        # Similarly, if we strip the final `.` from the glob, e.g.
+        #     "80.0".startswith("80.0") -> True
+        # then 80.10.0 will match 80.1.*:
+        #     "80.10".startswith("80.1") -> True
+        # We can either try to reverse engineer StrictVersion's intelligent
+        # version string manipulation, or just not cast `value` to
+        # `versionClass` if we're using `glob_op`.
+        value = versionClass(value)
+        # The glob operand doesn't match StrictVersion's strict version rules,
+        # whether we strip the `*` from it or not, so casting it as a
+        # `versionClass` will throw an exception unless we create a new
+        # StrictVersion based class that accepts those versions.
+        # Let's do a string comparison in `glob_op` instead.
+        operand = versionClass(operand)
     return opfunc(value, operand)
