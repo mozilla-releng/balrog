949bb6f12fa271e82c4d808a1430d632e5d8dc5a
Geoff Brown <gbrown@mozilla.com>
2022-05-24 11:17:53 -0600

    add pin.sh (#2533)
    * add pin.sh
    
    * exclude maintenance dir in manifest

---
+++ maintenance/README.md
@@ -0,0 +1,1 @@
+This directory contains helper scripts to maintain the balrog repo.
---
+++ maintenance/pin.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# We run this in balrog/. to pin all requirements.
+#
+# Usage: maintenance/pin.sh [<extra-pip-compile-multi-arguments>]
+#
+set -e
+set -x
+
+docker run -t -v $PWD:/src -w /src python:3.9 maintenance/pin-helper.sh $@
