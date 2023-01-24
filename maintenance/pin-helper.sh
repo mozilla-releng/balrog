61d3092f6ae98df4c8222ae6f22a56461f8d8865
Ben Hearsum (he/him) <ben@mozilla.com>
2023-01-24 10:30:01 -0500

    Fix up local tests in fresh environments (#2810)
    * Update maintenance script to run pip-compile-multi in a way that's compatible with local dev environments
    
    Local dev environments need to have `setuptools` pinned (I honestly don't remember exactly why right now...but anything that uses Dockerfile.test complains about it if it's not at the moment. This means we need `--allow-unsafe` (which is actually going to become default pip behaviour in the future anyways....)
    
    I also added `--backtracking` because when I tried to repin I got a complaint about dependency resolution with `pyjwt` /shrug.
    
    * s/whitelist_externals/allowlist_externals/ in tox.ini & update requirements
    
    Both of these are necessary to get plain `tox` invocations of tests working again AFAICT. (If you have a much older docker image they may still be working, but it certainly doesn't with a fresh one.)

--- maintenance/pin-helper.sh
+++ maintenance/pin-helper.sh
@@ -12,12 +12,16 @@ pip install pip-compile-multi
 
 apt-get update
 
-ARGS="-g base -g docs -g test -g local"
+# --backtracing is required to work around issues with pyjwt, eg:
+# Using legacy resolver. Consider using backtracking resolver with `--resolver=backtracking`.
+# Could not find a version that matches pyjwt[crypto]==2.4.0,>=2.6.0 from https://files.pythonhosted.org/packages/1c/fb/b82e9601b00d88cf8bbee1f39b855ae773f9d5bcbcedb3801b2f72460696/PyJWT-2.4.0-py3-none-any.whl (from -r requirements/base.in (line 29))
+# --allow-unsafe is required because local dev environments need all deps hashed to install properly
+ARGS="-g base -g docs -g test -g local --backtracking --allow-unsafe"
 pip-compile-multi -o "$SUFFIX" $ARGS $EXTRA_PCM_ARGS
 sed -i 's/^repoze-lru/repoze.lru/' requirements/*.txt
 chmod 644 requirements/*.txt
 
 cd agent
-ARGS="-g base -g test -g local"
+ARGS="-g base -g test -g local --backtracking --allow-unsafe"
 pip-compile-multi -o "$SUFFIX" $ARGS $EXTRA_PCM_ARGS
 chmod 644 requirements/*.txt
