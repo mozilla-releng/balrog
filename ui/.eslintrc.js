c7a4a591fb387bf11ba1277cd89be66cd40f1f74
varun <varunshekhar1104@gmail.com>
2023-10-17 19:08:13 +0530

    fixing lint errors

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,32 +1,3 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+const neutrino = require('neutrino');
+
+module.exports = neutrino().eslintrc();
