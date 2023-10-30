46b4a3a79a7c28f5258b47e093eec3e9ca919e60
Michelle Mounde <mitchmounde@gmail.com>
2023-10-30 13:20:15 +0300

    Fix lint errors

--- ui/src/utils/rules.js
+++ ui/src/utils/rules.js
@@ -23,7 +23,9 @@ const ruleMatchesChannel = (rule, channel) => {
   // because the scheduled change channel on deletions is null
   // and the rule channel is a truthy value that might not equal 'channel'
   const scChannelMatches = rule.scheduledChange
-    ? (rule.channel && ruleChannelMatches && rule.scheduledChange.channel === null) ||
+    ? (rule.channel &&
+        ruleChannelMatches &&
+        rule.scheduledChange.channel === null) ||
       (!rule.channel && rule.scheduledChange.channel === null) ||
       rule.scheduledChange.channel === '' ||
       rule.scheduledChange.channel === channel ||
