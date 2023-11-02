b28df5907a38bba2782af5141d399d3a3e89a845
AMUZY <nnamdiamu5@gmail.com>
2023-11-02 22:04:47 +0100

    refactored the matchesGlob nested function

--- ui/src/utils/rules.js
+++ ui/src/utils/rules.js
@@ -1,9 +1,13 @@
+import getIndexOfSubStr from './getIndexOfSubStr';
+
 // test me in local dev too!
 const ruleMatchesChannel = (rule, channel) => {
   // we support globs at the end of a channel only, hence
   // splitting and taking the first part
   const matchesGlob = (r, c) =>
-    r && r.includes('*') && c.startsWith(r.split('*')[0]);
+    (r && r.includes('*') && c.startsWith(r.split('*')[0])) ||
+    c.substring(0, getIndexOfSubStr(c, '-', 1)) === r ||
+    `${c.substring(0, getIndexOfSubStr(c, '-', 1))}*` === r;
   const ruleChannelMatches =
     // empty or absent channel matches anything
     // however, a rule could also be non-existent
@@ -19,9 +23,9 @@ const ruleMatchesChannel = (rule, channel) => {
   // we never want this to match, otherwise all rules
   // without scheduled changes will always match any filter
   const scChannelMatches = rule.scheduledChange
-    ? rule.scheduledChange.channel === null ||
-      rule.scheduledChange.channel === '' ||
-      rule.scheduledChange.channel === channel ||
+    ? (rule.scheduledChange.channel === null ||
+        rule.scheduledChange.channel === '' ||
+        rule.scheduledChange.channel === channel) &&
       matchesGlob(rule.scheduledChange.channel, channel)
     : false;
 
