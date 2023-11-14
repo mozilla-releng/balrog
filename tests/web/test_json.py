eb2c4b3cdccb63bb488814513d6a790967bdec89
Michelle Mounde <mitchmounde@gmail.com>
2023-11-14 11:18:53 +0300

    Remove response assertion in test

--- tests/web/test_json.py
+++ tests/web/test_json.py
@@ -235,14 +235,13 @@ def testGuardianResponse(client, version, buildTarget, channel, code, response):
 
 @pytest.mark.usefixtures("appconfig", "guardian_db", "disable_errorhandler", "mock_autograph")
 @pytest.mark.parametrize(
-    "version,buildTarget,channel,code,response,jsonString",
+    "version,buildTarget,channel,code,jsonString",
     [
         (
             "0.4.0.0",
             "WINNT_x86_64",
             "release",
             200,
-            {"required": True, "url": "https://good.com/0.5.0.0.msi", "version": "0.5.0.0", "hashFunction": "sha512", "hashValue": "abcdef"},
             '{"hashFunction":"sha512","hashValue":"abcdef","required":true,"url":"https://good.com/0.5.0.0.msi","version":"0.5.0.0"}'
         ),
         (
@@ -250,7 +249,6 @@ def testGuardianResponse(client, version, buildTarget, channel, code, response):
             "WINNT_x86_64",
             "release",
             200,
-            {"required": True, "url": "https://good.com/1.0.0.0.msi", "version": "1.0.0.0", "hashFunction": "sha512", "hashValue": "mnopqr"},
             '{"hashFunction":"sha512","hashValue":"mnopqr","required":true,"url":"https://good.com/1.0.0.0.msi","version":"1.0.0.0"}'
         ),
         (
@@ -258,23 +256,21 @@ def testGuardianResponse(client, version, buildTarget, channel, code, response):
             "WINNT_x86_64",
             "release",
             200,
-            {"required": True, "url": "https://good.com/1.0.0.0.msi", "version": "1.0.0.0", "hashFunction": "sha512", "hashValue": "mnopqr"},
             '{"hashFunction":"sha512","hashValue":"mnopqr","required":true,"url":"https://good.com/1.0.0.0.msi","version":"1.0.0.0"}'
         ),
-        ("1.0.0.0", "WINNT_x86_64", "release", 404, {}, "{}"),
-        ("0.6.0.0", "Linux_x86_64", "release", 404, {}, "{}"),
-        ("0.6.0.0", "WINNT_x86_64", "beta", 404, {}, "{}"),
+        ("1.0.0.0", "WINNT_x86_64", "release", 404, "{}"),
+        ("0.6.0.0", "Linux_x86_64", "release", 404, "{}"),
+        ("0.6.0.0", "WINNT_x86_64", "beta", 404, "{}"),
         # This shouldn't match because the rule on the alpha channel contains fields not used by this type of update query.
-        ("0.6.0.0", "WINNT_x86_64", "alpha", 404, {}, "{}"),
-        ("0.6.0.0", "Darwin_x86_64", "evilrelease", 200, {}, "{}"),
+        ("0.6.0.0", "WINNT_x86_64", "alpha", 404, "{}"),
+        ("0.6.0.0", "Darwin_x86_64", "evilrelease", 200, "{}"),
     ],
 )
-def testGuardianResponseWithCanonicalJson(client, version, buildTarget, channel, code, response, jsonString):
+def testGuardianResponseWithCanonicalJson(client, version, buildTarget, channel, code, jsonString):
     ret = client.get(f"/json/1/Guardian/{version}/{buildTarget}/{channel}/update.json")
     assert ret.status_code == code
     if code == 200:
         assert ret.mimetype == "application/json"
-        assert ret.get_json() == response
         assert ret.text == jsonString
         assert ret.headers["Content-Signature"] == "x5u=https://this.is/a.x5u; p384ecdsa=abcdef"
 
