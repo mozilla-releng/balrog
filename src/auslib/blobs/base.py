1a6d6fd64569d8ed9ad04f332159a1469c9fe04c
Julien Cristau <jcristau@mozilla.com>
2023-11-28 11:55:51 +0100

    Fix jsonschema deprecation warning missed in #2665

--- src/auslib/blobs/base.py
+++ src/auslib/blobs/base.py
@@ -148,7 +148,7 @@ class Blob(dict):
     def validate(self, product, allowlistedDomains):
         """Raises a BlobValidationError if the blob is invalid."""
         self.log.debug("Validating blob %s" % self)
-        validator = jsonschema.Draft4Validator(self.getSchema(), format_checker=jsonschema.draft4_format_checker)
+        validator = jsonschema.Draft4Validator(self.getSchema(), format_checker=jsonschema.Draft4Validator.FORMAT_CHECKER)
         # Normal usage is to use .validate(), but errors raised by it return
         # a massive error message that includes the entire blob, which is way
         # too big to be useful in the UI. Instead, we iterate over the
