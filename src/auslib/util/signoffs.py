18b6b7c6cbc32a686b21fe6928ba975c9edea71d
Michelle Mounde <mitchmounde@gmail.com>
2024-01-19 17:24:07 +0300

    Update signoff requirements serialization to check for permission or role

--- src/auslib/util/signoffs.py
+++ src/auslib/util/signoffs.py
@@ -1,8 +1,8 @@
 def serialize_signoff_requirements(requirements):
     dct = {}
     for rs in requirements:
-        signoff_check = "role" if "role" in rs else "permission"
-        signoffs_required = max(dct.get(rs[signoff_check], 0), rs["signoffs_required"])
-        dct[rs[signoff_check]] = signoffs_required
+        signoff_verifier = "role" if "role" in rs else "permission"
+        signoffs_required = max(dct.get(rs[signoff_verifier], 0), rs["signoffs_required"])
+        dct[rs[signoff_verifier]] = signoffs_required
 
     return dct
