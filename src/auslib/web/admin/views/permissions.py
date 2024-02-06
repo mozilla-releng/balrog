349cb1aa3f7df581a7344bc7b7c5e0b432b69ec8
Michelle Mounde <mitchmounde@gmail.com>
2024-02-06 11:39:29 +0300

    Add transaction and exception handlers to permission functions

--- src/auslib/web/admin/views/permissions.py
+++ src/auslib/web/admin/views/permissions.py
@@ -62,7 +62,9 @@ def get_specific_user_permission(username, permission, changed_by):
 
 
 @requirelogin
-def put_specific_user_permission(username, permission, changed_by, transaction):
+@transactionHandler
+@handleGeneralExceptions("PUT")
+
     try:
         if dbo.permissions.getUserPermissions(username, changed_by, transaction).get(permission):
             # Existing Permission
@@ -99,7 +101,9 @@ def put_specific_user_permission(username, permission, changed_by, transaction):
 
 
 @requirelogin
-def post_specific_user_permission(username, permission, changed_by, transaction):
+@transactionHandler
+@handleGeneralExceptions("POST")
+
     if not dbo.permissions.getUserPermissions(username, changed_by, transaction=transaction).get(permission):
         return problem(status=404, title="Not Found", detail="Requested user permission" " %s not found for %s" % (permission, username))
     try:
@@ -127,7 +131,9 @@ def post_specific_user_permission(username, permission, changed_by, transaction)
 
 
 @requirelogin
-def delete_specific_user_permission(username, permission, changed_by, transaction):
+@transactionHandler
+@handleGeneralExceptions("DELETE")
+
     if not dbo.permissions.getUserPermissions(username, changed_by, transaction=transaction).get(permission):
         return problem(404, "Not Found", "Requested user permission" " %s not found for %s" % (permission, username))
     try:
@@ -251,6 +257,7 @@ class PermissionScheduledChangeHistoryView(ScheduledChangeHistoryView):
 
 @requirelogin
 @transactionHandler
+@handleGeneralExceptions("PUT")
 def put_user_role(username, role, changed_by, transaction):
     """/users/:username/roles/:role"""
 
@@ -267,6 +274,7 @@ def put_user_role(username, role, changed_by, transaction):
 
 @requirelogin
 @transactionHandler
+@handleGeneralExceptions("DELETE")
 def delete_user_role(username, role, data_version, changed_by, transaction):
     roles = [r["role"] for r in dbo.permissions.getUserRoles(username)]
     if role not in roles:
