2cccd39f978975670b2f1c9d657054a33a3d8b64
Gabriel Bustamante <gbustamante@mozilla.com>
2024-03-19 14:07:10 -0500

    Lint and fix Jest JS configuration files

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,32 +1,22 @@
-process.env.NODE_ENV = process.env.NODE_ENV || "test";
+process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
 module.exports = {
   rootDir: `${__dirname}`,
-  moduleDirectories: [
-    "node_modules"
-  ],
-  moduleFileExtensions: [
-    "web.jsx",
-    "web.js",
-    "wasm",
-    "jsx",
-    "js",
-    "json"
-  ],
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['web.jsx', 'web.js', 'wasm', 'jsx', 'js', 'json'],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__jest__/fileMock.js",
-    "\\.(css|less|sass|scss)$": "<rootDir>/__jest__/styleMock.js",
-    "^react-native$": "<rootDir>/node_modules/react-native-web",
-    "^react-dom$": "<rootDir>/node_modules/@hot-loader/react-dom"
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__jest__/fileMock.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/__jest__/styleMock.js',
+    '^react-native$': '<rootDir>/node_modules/react-native-web',
+    '^react-dom$': '<rootDir>/node_modules/@hot-loader/react-dom',
   },
   bail: true,
-  collectCoverageFrom: [
-    "src/**/*.{mjs,jsx,js}"
-  ],
-  testEnvironment: "jsdom",
-  testRegex: "test/.*(_test|_spec|\\.test|\\.spec)\\.(mjs|jsx|js)$",
+  collectCoverageFrom: ['src/**/*.{mjs,jsx,js}'],
+  testEnvironment: 'jsdom',
+  testRegex: 'test/.*(_test|_spec|\\.test|\\.spec)\\.(mjs|jsx|js)$',
   verbose: false,
   transform: {
-    "\\.(mjs|jsx|js)$": "<rootDir>/__jest__/transformer.js"
-  }
-}
+    '\\.(mjs|jsx|js)$': '<rootDir>/__jest__/transformer.js',
+  },
+};
