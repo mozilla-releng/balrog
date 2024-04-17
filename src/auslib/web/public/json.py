d36a7d53ec9a8eb7038e2e4451b004c182c2e462
Heitor Neiva <hneiva@mozilla.com>
2024-04-16 17:33:14 -0700

    Bug 1882147 - Support legacy keys for signing json responses

--- src/auslib/web/public/json.py
+++ src/auslib/web/public/json.py
@@ -1,10 +1,35 @@
 import json
 
+import semver
 from flask import Response
 from flask import current_app as app
 
 from auslib.AUS import FORCE_FALLBACK_MAPPING, FORCE_MAIN_MAPPING
 from auslib.web.public.helpers import AUS, get_aus_metadata_headers, get_content_signature_headers, with_transaction
+
+# Map of products and version.
+# Uses legacy key if requested version is < value set here.
+LEGACY_PRODUCT_VERSION_MAPPING = {"FirefoxVPN": "2.22.0"}
+
+
+def check_for_legacy_key(parameters):
+    product = parameters.get("product")
+    version = parameters.get("version")
+    # Skip if product is not in the mapping or version is not provided
+    if product not in LEGACY_PRODUCT_VERSION_MAPPING or not version:
+        return False
+
+    try:
+        limit_version = semver.version.Version.parse(LEGACY_PRODUCT_VERSION_MAPPING[product])
+        version = semver.version.Version.parse(version)
+        print(version, limit_version)
+        print(version < limit_version)
+        if version < limit_version:
+            return True
+    except semver.VersionException:
+        # Likely a malformed version string - ignore legacy key and sign with latest available
+        pass
+    return False
 
 
 @with_transaction
@@ -19,7 +44,7 @@ def get_update(transaction, **parameters):
 
     response = json.dumps(release.getResponse(parameters, app.config["ALLOWLISTED_DOMAINS"]))
 
-    headers.update(get_content_signature_headers(response, ""))
+    headers.update(get_content_signature_headers(response, "", check_for_legacy_key(parameters)))
 
     return Response(response=response, status=200, headers=headers, mimetype="application/json")
 
