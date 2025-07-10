517daea1b34690317e8a61e4f32e2c77c0220c61
Ben Hearsum (he/him) <ben@mozilla.com>
2025-07-10 05:31:28 -0400

    feat: use uv for dependency resolution in pin-helper.sh (#3458)
    pip-compile-multi gained support for this a few weeks ago. It looks like there's no downsides to using it?

--- maintenance/pin-helper.sh
+++ maintenance/pin-helper.sh
@@ -9,7 +9,7 @@ fi
 
 PIP="pip --disable-pip-version-check install --root-user-action ignore"
 $PIP --upgrade 'pip<25.1'
-$PIP 'pip-compile-multi<3'
+$PIP 'pip-compile-multi<3' 'uv'
 
 apt-get update
 
@@ -18,10 +18,10 @@ apt-get update
 # Could not find a version that matches pyjwt[crypto]==2.4.0,>=2.6.0 from https://files.pythonhosted.org/packages/1c/fb/b82e9601b00d88cf8bbee1f39b855ae773f9d5bcbcedb3801b2f72460696/PyJWT-2.4.0-py3-none-any.whl (from -r requirements/base.in (line 29))
 # --allow-unsafe is required because local dev environments need all deps hashed to install properly
 ARGS="-g base -g docs -g test -g local --backtracking --allow-unsafe"
-pip-compile-multi -o "$SUFFIX" $ARGS $EXTRA_PCM_ARGS
+pip-compile-multi --uv -o "$SUFFIX" $ARGS $EXTRA_PCM_ARGS
 chmod 644 requirements/*.txt
 
 cd agent
 ARGS="-g base -g test -g local --backtracking --allow-unsafe"
-pip-compile-multi -o "$SUFFIX" $ARGS $EXTRA_PCM_ARGS
+pip-compile-multi --uv -o "$SUFFIX" $ARGS $EXTRA_PCM_ARGS
 chmod 644 requirements/*.txt
