5b864fce35ca5f104979255a45fcebecf4f18ba4
Bastien Orivel <borivel@mozilla.com>
2025-07-23 17:56:06 +0200

    Remove html-loader (#3482)
    We don't import any html file anywhere so it's not used. It's just a
    remnant from the neutrino migration because neutrino was always emitting
    that rule even if it was not necessary.

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -81,17 +81,6 @@ module.exports = (_, { mode }) => {
     module: {
       rules: [
         {
-          test: /\.html$/,
-          use: [
-            {
-              loader: 'html-loader',
-              options: {
-                attrs: ['img:src', 'link:href'],
-              },
-            },
-          ],
-        },
-        {
           test: /\.m?js/,
           resolve: {
             fullySpecified: false,
