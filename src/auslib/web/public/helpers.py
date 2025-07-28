a6e8a050864216e245335896dc01fe82930517ec
Ben Hearsum <ben@mozilla.com>
2025-07-28 15:20:30 -0400

    feat: warm caches during app startup
    This code runs before the public app begins accepting requests, which should mean that we don't need to hit the database very much after beginning to serve requests. Doing this at start-up also means we fetch the releases sequentially, which should (slightly) spread the load out more.
    
    I verified locally that the app is incapable of responding to requests until `public.py` finishes executing by adding a `time.sleep` to the end of it. While the sleep was running requests to the app hung. This means that `/__heartbeat__` requests won't complete successfully until caches have been warmed up.
    
    Hopefully this will help with database load during scaling events. See: https://mozilla-hub.atlassian.net/browse/SVCSE-3425

--- src/auslib/web/public/helpers.py
+++ src/auslib/web/public/helpers.py
@@ -5,6 +5,7 @@ from flask import current_app as app
 
 from auslib.AUS import AUS
 from auslib.global_state import cache, dbo
+from auslib.services.releases import get_release_blob
 from auslib.util.autograph import make_hash, sign_hash
 
 log = logging.getLogger(__name__)
@@ -50,6 +51,26 @@ def get_content_signature_headers(content, product):
         headers = {"Content-Signature": f"x5u={x5u}; p384ecdsa={signature}"}
         log.debug("Added header: %s" % headers)
     return headers
+
+
+def warm_caches():
+    """Fetch all releases pointed at by a rule. This is intended to run at app
+    startup, to ensure that it has all necessary information is in memory before
+    it begins serving requests."""
+    mapped_releases = set()
+    for rule in dbo.rules.select(columns=[dbo.rules.mapping, dbo.rules.fallbackMapping], distinct=True):
+        if rule["mapping"]:
+            mapped_releases.add(rule["mapping"])
+        if rule["fallbackMapping"]:
+            mapped_releases.add(rule["fallbackMapping"])
+
+    for release in mapped_releases:
+        blob = get_release_blob(release)
+        if not blob:
+            continue
+
+        for referenced_release in blob.getReferencedReleases():
+            get_release_blob(referenced_release)
 
 
 AUS = AUS()
