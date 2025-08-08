18f7a8053e28bddb8505882b36f3bc8ce9cc6258
Bastien Orivel <borivel@mozilla.com>
2025-08-08 15:42:27 +0200

    Replace unidiff usage with diff and update the latter (#3511)
    * Replace `unidiff` usage with `diff`
    
    We already depend on `diff for the releases diffs. This was just a
    second dependency used to do the same thing, and unidiff depends on diff
    (an outdated version at that), so just remove it.
    
    I ran into react-diff-view not liking createPatch adding `Index:
    file\n=============================` at the top even though that seems
    to be valid in a unified diff (patch(1) ate it without complaining) so I
    just those two off. If those lines ever come to disappear then we'll eat
    the entire header instead which will make react-diff-view very angry so
    I'm not worried about eating two lines off a diff by mistake.
    
    The other difference is that when the files match (oldText, newText are
    empty strings), unidiff used to return an empty string but createPatch
    returns a header which makes parseDiff throw some exception, so we
    catch this and return an empty string which is what it did before
    
    * Update diff

--- ui/src/components/DiffRule/index.jsx
+++ ui/src/components/DiffRule/index.jsx
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/styles';
+import { createPatch } from 'diff';
 import React, { useMemo } from 'react';
 import { Diff, Hunk, parseDiff } from 'react-diff-view';
-import { diffLines, formatLines } from 'unidiff';
 import 'react-diff-view/style/index.css';
 import { RULE_DIFF_PROPERTIES } from '../../utils/constants';
 import getDiff from '../../utils/diff';
@@ -34,9 +34,25 @@ function DiffRule(props) {
   );
   const diff = useMemo(() => {
     const [oldText, newText] = getDiff(diffedProperties, firstRule, secondRule);
-    const diffText = formatLines(diffLines(oldText, newText), {
-      context: 0,
-    });
+
+    if (!oldText && !newText) {
+      return '';
+    }
+
+    const patchText = createPatch(
+      'rules',
+      oldText,
+      newText,
+      undefined,
+      undefined,
+      {
+        context: 0,
+      },
+    );
+
+    // createPatch adds `Index: filename\n==========\n` at the top which `parseDiff` does not like
+    const diffText = patchText.split('\n').slice(2).join('\n');
+
     const [diff] = parseDiff(diffText, { nearbySequences: 'zip' });
 
     return diff;
