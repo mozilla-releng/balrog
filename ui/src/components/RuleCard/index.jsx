mozilla-releng/balrog

commit c3f865c7229f128fb99de73e66891f76d918ff4e

Update react, react-router-dom, MUI, node... (#3525)
This was something else... All of these very very tied together so I
couldn't update separately without much pain.

The main changes just like shipit are:

- The router API changed drastically. Those changes are at least very
  easy to test.
- The datetime picker was a **pain**. Instead of being split from the
  input and setting the value on validation it's now tied into the input
  value so we have to keep an intermediate state as changing the value
  of the rewind input on the rules page redraws the input too which
  makes the picker close. That means that you could select one component
  at a time... Not very user friendly. On the bright side, adding that
  state fixed the issue that if you tried to type a date in yourself
  you'd get a "nope, invalid date" after the first character and a page
  refresh for it...
- MUI changes all around... I'm tired boss... I tested every page that I
  knew how to find and every feature that I knew about but I probably
  missed things... Hopefully if I did then the UI isn't broken to the
  point of being unusable.

ui/src/components/RuleCard/index.jsx
@@ -1,20 +1,19 @@
 import { withAuth0 } from '@auth0/auth0-react';
-import Avatar from '@material-ui/core/Avatar';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Chip from '@material-ui/core/Chip';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Divider from '@material-ui/core/Divider';
-import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Tooltip from '@material-ui/core/Tooltip';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Chip from '@mui/material/Chip';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/GridLegacy';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 import classNames from 'classnames';
 import { formatDistanceStrict } from 'date-fns';
 import DeleteIcon from 'mdi-react/DeleteIcon';
@@ -23,6 +22,7 @@ import PlusCircleIcon from 'mdi-react/PlusCircleIcon';
 import UpdateIcon from 'mdi-react/UpdateIcon';
 import { bool, func } from 'prop-types';
 import React, { Fragment, useState } from 'react';
+import { makeStyles } from 'tss-react/mui';
 import { RULE_DIFF_PROPERTIES } from '../../utils/constants';
 import getDiffedProperties from '../../utils/getDiffedProperties';
 import getIndexOfSubStr from '../../utils/getIndexOfSubStr';
@@ -32,7 +32,7 @@ import Button from '../Button';
 import DiffRule from '../DiffRule';
 import SignoffSummary from '../SignoffSummary';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
   root: {
     '& h2, & h4': {
       '& .anchor-link-style': {
@@ -44,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
       '&:hover .anchor-link-style': {
         display: 'inline-block',
         opacity: 1,
-        color: theme.palette.text.hint,
+        color: theme.palette.text.disabled,
         '&:hover': {
           color: theme.palette.text.secondary,
         },
@@ -68,7 +68,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1),
   },
   noDiffedPropertiesText: {
-    padding: `0 ${theme.spacing(1)}px`,
+    padding: `0 ${theme.spacing(1)}`,
   },
   listItem: {
     paddingTop: 0,
@@ -81,7 +81,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'flex-end',
   },
   changesTitle: {
-    padding: `0 ${theme.spacing(1)}px`,
+    padding: `0 ${theme.spacing(1)}`,
   },
   diff: {
     fontSize: theme.typography.body2.fontSize,
@@ -102,7 +102,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   divider: {
-    margin: `${theme.spacing(1)}px`,
+    margin: theme.spacing(1),
     backgroundColor: theme.palette.grey[200],
   },
   changesHeader: {
@@ -168,7 +168,7 @@ function RuleCard({
 }) {
   const [open, setOpen] = useState(false);
   const [seeMore, setSeeMore] = useState('...see more');
-  const classes = useStyles();
+  const { classes } = useStyles();
   const requiresSignoff =
     rule.scheduledChange &&
     Object.keys(rule.scheduledChange.required_signoffs).length > 0;
@@ -247,7 +247,7 @@ function RuleCard({
         style={{
           display: 'flex',
           flexFlow: 'column',
-          color: 'white',
+          color: '#ffffff',
           fontSize: '14px',
           lineHeight: '16px',
           margin: '2px 4px',
@@ -344,7 +344,7 @@ function RuleCard({
                 }}
               >
                 <Tooltip title="Revisions">
-                  <IconButton>
+                  <IconButton size="large">
                     <HistoryIcon />
                   </IconButton>
                 </Tooltip>

ui/src/views/Rules/ListRules/index.jsx
@@ -1,23 +1,23 @@
 import { withAuth0 } from '@auth0/auth0-react';
-import Box from '@material-ui/core/Box';
-import Checkbox from '@material-ui/core/Checkbox';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Drawer from '@material-ui/core/Drawer';
-import Fab from '@material-ui/core/Fab';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormLabel from '@material-ui/core/FormLabel';
-import IconButton from '@material-ui/core/IconButton';
-import MenuItem from '@material-ui/core/MenuItem';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Switch from '@material-ui/core/Switch';
-import TextField from '@material-ui/core/TextField';
-import Tooltip from '@material-ui/core/Tooltip';
-import Alert from '@material-ui/lab/Alert';
-import AlertTitle from '@material-ui/lab/AlertTitle';
-import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
-import { makeStyles, useTheme } from '@material-ui/styles';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import CircularProgress from '@mui/material/CircularProgress';
+import Drawer from '@mui/material/Drawer';
+import Fab from '@mui/material/Fab';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import Switch from '@mui/material/Switch';
+import { useTheme } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 import classNames from 'classnames';
 import { addSeconds } from 'date-fns';
 import CloseIcon from 'mdi-react/CloseIcon';
@@ -33,6 +33,8 @@ import React, {
   useRef,
   useState,
 } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { makeStyles } from 'tss-react/mui';
 import Dashboard from '../../../components/Dashboard';
 import DateTimePicker from '../../../components/DateTimePicker';
 import DialogAction from '../../../components/DialogAction';
@@ -85,7 +87,7 @@ import { ruleMatchesRequiredSignoff } from '../../../utils/requiredSignoffs';
 import { ruleMatchesChannel } from '../../../utils/rules';
 
 const ALL = 'all';
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
   fab: {
     ...theme.mixins.fab,
     right: theme.spacing(12),
@@ -95,14 +97,14 @@ const useStyles = makeStyles((theme) => ({
     top: 64,
     right: 0,
     left: 0,
-    padding: `${theme.spacing(4)}px ${theme.spacing(4)}px 0`,
+    padding: `${theme.spacing(4)} ${theme.spacing(4)} 0`,
     display: 'flex',
     justifyContent: 'center',
     position: 'fixed',
     zIndex: 2,
   },
   checkbox: {
-    padding: `0 ${theme.spacing(1)}px`,
+    padding: `0 ${theme.spacing(1)}`,
   },
   checkboxLabel: {
     textAlign: 'center',
@@ -143,10 +145,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function ListRules(props) {
-  const classes = useStyles();
+  const { classes } = useStyles();
   const theme = useTheme();
   const username = props.auth0.user?.email || '';
-  const { search, hash } = props.location;
+  const { search, hash } = useLocation();
+  const navigate = useNavigate();
   const query = parse(search.slice(1));
   const hashQuery = parse(hash.replace('#', ''));
   const {
@@ -306,7 +309,7 @@ function ListRules(props) {
       channel,
     };
 
-    props.history.push(`/rules${stringify(qs, { addQueryPrefix: true })}`);
+    navigate(`/rules${stringify(qs, { addQueryPrefix: true })}`);
   };
 
   const handleShowOnlyScheduledChangesChange = ({ target: { checked } }) => {
@@ -315,7 +318,7 @@ function ListRules(props) {
       onlyScheduledChanges: checked ? 1 : undefined,
     };
 
-    props.history.push(`/rules${stringify(qs, { addQueryPrefix: true })}`);
+    navigate(`/rules${stringify(qs, { addQueryPrefix: true })}`);
   };
 
   const handleSignoffRoleChange = ({ target: { value } }) =>
@@ -569,7 +572,7 @@ function ListRules(props) {
       onlyScheduledChanges: undefined,
     };
 
-    props.history.push(`/rules${stringify(qs, { addQueryPrefix: true })}`);
+    navigate(`/rules${stringify(qs, { addQueryPrefix: true })}`);
   };
 
   const handleDialogError = (error) => {
@@ -1164,31 +1167,32 @@ function ListRules(props) {
   };
 
   const getRowHeight = (index) => {
+    const spacingPx = parseInt(theme.spacing(1), 10);
     const rule = filteredRulesWithScheduledChanges[index];
     const currentRule = rulesWithScheduledChanges.find(
       (r) => r.rule_id === rule.rule_id,
     );
     const hasScheduledChanges = Boolean(rule.scheduledChange);
     // Padding top and bottom included
-    const listPadding = theme.spacing(1);
+    const listPadding = spacingPx;
     const listItemTextMargin = 6;
     const diffRowHeight = remToPx(theme.typography.body2.fontSize) * 1.5;
     // <CardContent /> padding
-    let height = theme.spacing(2);
+    let height = 2 * spacingPx;
 
     // actions row
-    height += buttonHeight + theme.spacing(2);
+    height += buttonHeight + 2 * spacingPx;
 
     if (!hasScheduledChanges || rule.scheduledChange.change_type !== 'insert') {
       height +=
         Math.max(
           // avatar height
-          theme.spacing(4),
+          4 * spacingPx,
           // product:channel header height
           h6TextHeight,
           // revisions icon
-          theme.spacing(3) + 24,
-        ) + theme.spacing(1); // top padding
+          3 * spacingPx + 24,
+        ) + spacingPx; // top padding
 
       // != checks for both null and undefined
       const keys = Object.keys(rule).filter((key) => rule[key] != null);
@@ -1232,7 +1236,7 @@ function ListRules(props) {
 
     if (hasScheduledChanges) {
       // row with the chip label
-      height += Math.max(subtitle1TextHeight(), theme.spacing(3));
+      height += Math.max(subtitle1TextHeight(), 3 * spacingPx);
 
       if (rule.scheduledChange.change_type === 'delete') {
         // row with "all properties will be deleted"
@@ -1251,9 +1255,7 @@ function ListRules(props) {
         // horizontal scroller (rough estimate;
         // sometimes there are no scroller as well)
         height +=
-          diffedProperties.length * diffRowHeight +
-          theme.spacing(1) +
-          theme.spacing(2);
+          diffedProperties.length * diffRowHeight + spacingPx + 2 * spacingPx;
       }
 
       if (
@@ -1261,7 +1263,7 @@ function ListRules(props) {
         rule.scheduledChange.change_type === 'update'
       ) {
         // divider
-        height += theme.spacing(2) + 1;
+        height += 2 * spacingPx + 1;
       }
 
       if (Object.keys(rule.scheduledChange.required_signoffs).length > 0) {
@@ -1274,17 +1276,17 @@ function ListRules(props) {
         const signoffRows = Math.max(requiredRoles, nSignoffs);
 
         // Padding above the summary
-        height += theme.spacing(2);
+        height += 2 * spacingPx;
 
         // The "Requires Signoff From" title and the margin beneath it
-        height += signoffSummarylistSubheaderTextHeight + theme.spacing(0.5);
+        height += signoffSummarylistSubheaderTextHeight + 0.5 * spacingPx;
 
         // Space for however many rows exist.
         height += signoffRows * body2TextHeight();
       }
     } else if (showRewindDiff) {
       // row with the historical changes title
-      height += Math.max(subtitle1TextHeight(), theme.spacing(3));
+      height += Math.max(subtitle1TextHeight(), 3 * spacingPx);
 
       if (!currentRule) {
         // row with "rule was deleted"
@@ -1304,18 +1306,16 @@ function ListRules(props) {
           // horizontal scroller (rough estimate;
           // sometimes there are no scroller as well)
           height +=
-            diffedProperties.length * diffRowHeight +
-            theme.spacing(1) +
-            theme.spacing(2);
+            diffedProperties.length * diffRowHeight + spacingPx + 2 * spacingPx;
         }
       }
 
       // divider
-      height += theme.spacing(2) + 1;
+      height += 2 * spacingPx + 1;
     }
 
     // space below the card (margin)
-    height += theme.spacing(4);
+    height += 4 * spacingPx;
 
     return height;
   };
@@ -1417,8 +1417,8 @@ function ListRules(props) {
       }
     >
       {isLoading && (
-        <Box style={{ textAlign: 'center' }}>
-          <CircularProgress loading />
+        <Box sx={{ textAlign: 'center' }}>
+          <CircularProgress />
         </Box>
       )}
       {error && <ErrorPanel error={error} />}
@@ -1434,23 +1434,22 @@ function ListRules(props) {
               helperText={rewindDateError}
               onDateTimeChange={handleRewindDateTimeChange}
               value={rewindDate}
-              InputProps={
-                rewindDate && {
-                  endAdornment: (
-                    <IconButton
-                      onClick={() => handleRewindDateTimeChange(null)}
-                      disabled={!rewindDate}
-                      style={{ order: 1 }}
-                      color="disabled"
-                    >
-                      <CloseIcon />
-                    </IconButton>
-                  ),
-                }
-              }
-              InputAdornmentProps={{
-                position: 'start',
-                style: { order: 2, marginLeft: 0 },
+              slotProps={{
+                textField: {
+                  InputProps: {
+                    endAdornment: rewindDate && (
+                      <IconButton
+                        onClick={() => handleRewindDateTimeChange(null)}
+                        disabled={!rewindDate}
+                        color="default"
+                        size="small"
+                        sx={{ marginRight: 1 }}
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    ),
+                  },
+                },
               }}
             />
             <FormControl className={classes.checkbox}>

ui/src/components/AutoCompleteText/index.jsx
@@ -1,10 +1,10 @@
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import { makeStyles } from '@material-ui/styles';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 import { arrayOf, bool, func, object, string } from 'prop-types';
 import React from 'react';
+import { makeStyles } from 'tss-react/mui';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
   chip: {
     fontSize: '0.60rem',
     height: 'auto',
@@ -39,7 +39,7 @@ function AutoCompleteText({
   multi,
   ...props
 }) {
-  const classes = useStyles();
+  const { classes } = useStyles();
   const handleSingleChange = (_event, newValue) => {
     onValueChange(newValue || '');
   };

ui/src/components/Button/index.jsx
@@ -1,7 +1,10 @@
-import MuiButton from '@material-ui/core/Button';
-import { red } from '@material-ui/core/colors';
-import { createTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
+import MuiButton from '@mui/material/Button';
+import { red } from '@mui/material/colors';
+import {
+  createTheme,
+  StyledEngineProvider,
+  ThemeProvider,
+} from '@mui/material/styles';
 import { oneOf } from 'prop-types';
 import React from 'react';
 
@@ -15,9 +18,11 @@ const dangerTheme = createTheme({
 function Button({ color, ...rest }) {
   if (color === 'danger') {
     return (
-      <ThemeProvider theme={dangerTheme}>
-        <MuiButton color="primary" {...rest} />
-      </ThemeProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={dangerTheme}>
+          <MuiButton color="primary" {...rest} />
+        </ThemeProvider>
+      </StyledEngineProvider>
     );
   }
 

ui/src/components/Dashboard/index.jsx
@@ -1,10 +1,10 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import { bool, node, string } from 'prop-types';
 import React, { Fragment } from 'react';
-import { Helmet } from 'react-helmet';
+import { useLocation } from 'react-router-dom';
+import { makeStyles } from 'tss-react/mui';
 import { APP_BAR_HEIGHT, CONTENT_MAX_WIDTH } from '../../utils/constants';
 import Link from '../../utils/Link';
 import Button from '../Button';
@@ -12,7 +12,7 @@ import menuItems from './menuItems';
 import SettingsMenu from './SettingsMenu';
 import UserMenu from './UserMenu';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
   appbar: {
     height: APP_BAR_HEIGHT,
   },
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: CONTENT_MAX_WIDTH,
     height: '100%',
     margin: '0 auto',
-    padding: `${theme.spacing(12)}px ${APP_BAR_HEIGHT}px`,
+    padding: `${theme.spacing(12)} ${APP_BAR_HEIGHT}px`,
   },
   nav: {
     display: 'flex',
@@ -46,14 +46,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Dashboard(props) {
-  const classes = useStyles();
+  const { classes } = useStyles();
   const { title, children, disabled } = props;
+  const location = useLocation();
 
   return (
     <Fragment>
-      <Helmet>
-        <title>{title} - Balrog Admin</title>
-      </Helmet>
+      <title>{`${title} - Balrog Admin`}</title>
       <AppBar className={classes.appbar}>
         <Toolbar>
           <Typography
@@ -73,8 +72,8 @@ export default function Dashboard(props) {
                 className={disabled ? classes.disabledLink : classes.link}
                 nav
                 to={
-                  window.location.pathname === menuItem.path
-                    ? `${window.location.pathname}${window.location.search}`
+                  location.pathname === menuItem.path
+                    ? `${location.pathname}${location.search}`
                     : menuItem.path
                 }
               >

ui/src/components/DataTable/index.jsx
@@ -1,9 +1,9 @@
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
 import {
   arrayOf,
   func,

ui/src/components/DateTimePicker/index.jsx
@@ -1,32 +1,84 @@
-import DateFnsUtils from '@date-io/date-fns';
-import {
-  KeyboardDateTimePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
+import { renderTimeViewClock } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DateTimePicker as MUIDateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { func, instanceOf } from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function DateTimePicker({ value, onDateTimeChange, ...props }) {
-  function handleDateChange(date) {
-    onDateTimeChange(date);
-  }
+export default function DateTimePicker({
+  value,
+  onDateTimeChange,
+  disablePast,
+  fullWidth,
+  inputVariant,
+  error,
+  helperText,
+  slotProps,
+  ...props
+}) {
+  // We don't want to call onDateTimeChange before the datetime is fully selected so we have to use an intermediate
+  // state here...
+  const [internalValue, setInternalValue] = useState(value);
+
+  useEffect(() => {
+    setInternalValue(value);
+  }, [value]);
+
+  // disablePast on the input is too eager and will mark the input as invalid when the page loads
+  const minDateTime = disablePast
+    ? (() => {
+        const now = new Date();
+        return new Date(
+          now.getFullYear(),
+          now.getMonth(),
+          now.getDate(),
+          now.getHours(),
+          now.getMinutes(),
+          0,
+          0,
+        );
+      })()
+    : undefined;
+
+  const handleChange = (newValue) => {
+    setInternalValue(newValue);
+    if (newValue === null) {
+      onDateTimeChange(newValue);
+    }
+  };
+
+  const mergedSlotProps = {
+    textField: {
+      margin: 'normal',
+      fullWidth,
+      variant: inputVariant || 'outlined',
+      error,
+      helperText,
+      ...slotProps?.textField,
+    },
+    actionBar: {
+      actions: ['today', 'clear', 'accept'],
+      ...slotProps?.actionBar,
+    },
+    ...slotProps,
+  };
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <KeyboardDateTimePicker
-        margin="normal"
-        showTodayButton
-        value={value}
-        onChange={handleDateChange}
-        autoOk
-        minDateMessage="Date should not be in the past"
-        KeyboardButtonProps={{
-          'aria-label': 'change date',
-        }}
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <MUIDateTimePicker
+        value={internalValue}
+        onChange={handleChange}
+        onAccept={onDateTimeChange}
+        minDateTime={minDateTime}
         format="yyyy/MM/dd hh:mm a"
+        viewRenderers={{
+          hours: renderTimeViewClock,
+          minutes: renderTimeViewClock,
+        }}
+        slotProps={mergedSlotProps}
         {...props}
       />
-    </MuiPickersUtilsProvider>
+    </LocalizationProvider>
   );
 }
 
@@ -37,7 +89,7 @@ DateTimePicker.propTypes = {
    */
   onDateTimeChange: func.isRequired,
   /**
-   * The value of the picker.
+   * The value of the picker. Can be a Date object or null.
    */
-  value: instanceOf(Date).isRequired,
+  value: instanceOf(Date),
 };

commit 069766e5d9892a15bbef7f263591a41a19824d23

Pass the state correctly to the `Link` component
This fixes a regression from c3f865c7229f128fb99de73e66891f76d918ff4e
where updating react-router silently broke keeping some state across
navigation. The issue is that react router's API changed the state
property from being nested within the `to` object to being directly on
the routing components and the `to` component becoming a string. We
didn't see this at all because of the `Link` wrapper that was doing some
massaging on the `to` property when it wasn't a string, so we were just
silently discarding the state parameter.

I removed said massaging to since we're passing strings to `to`
everywhere now as we should.

Fixes #3547

ui/src/components/RuleCard/index.jsx
@@ -338,10 +338,8 @@ function RuleCard({
           action={
             !readOnly ? (
               <Link
-                to={{
-                  pathname: `/rules/${rule.rule_id}/revisions`,
-                  state: { rulesFilter },
-                }}
+                to={`/rules/${rule.rule_id}/revisions`}
+                state={{ rulesFilter }}
               >
                 <Tooltip title="Revisions">
                   <IconButton size="large">
@@ -871,14 +869,12 @@ function RuleCard({
           ) : (
             <Link
               className={classes.link}
-              to={{
-                pathname: rule.rule_id
+              to={
+                rule.rule_id
                   ? `/rules/duplicate/ruleId/${rule.rule_id}`
-                  : `/rules/duplicate/scId/${rule.scheduledChange.sc_id}`,
-                state: {
-                  rulesFilter,
-                },
-              }}
+                  : `/rules/duplicate/scId/${rule.scheduledChange.sc_id}`
+              }
+              state={{ rulesFilter }}
             >
               <Button color="secondary">Duplicate</Button>
             </Link>
@@ -890,14 +886,12 @@ function RuleCard({
           ) : (
             <Link
               className={classes.link}
-              to={{
-                pathname: rule.rule_id
+              to={
+                rule.rule_id
                   ? `/rules/${rule.rule_id}`
-                  : `/rules/create/${rule.scheduledChange.sc_id}`,
-                state: {
-                  rulesFilter,
-                },
-              }}
+                  : `/rules/create/${rule.scheduledChange.sc_id}`
+              }
+              state={{ rulesFilter }}
             >
               <Button color="secondary">Update</Button>
             </Link>

ui/src/utils/Link.jsx
@@ -9,8 +9,7 @@ import matchRoutes from './matchRoutes';
  * with pre-fetching capabilities.
  */
 export default function Link({ viewName, nav, to, ...props }) {
-  const path = typeof to === 'string' ? to : to.pathname;
-  const isPathAbsolute = URL.canParse(path);
+  const isPathAbsolute = URL.canParse(to);
   const Component = nav ? NavLink : RouterLink;
   const [prefetchFlag, setPrefetchFlag] = useState(false);
 
@@ -20,7 +19,7 @@ export default function Link({ viewName, nav, to, ...props }) {
     }
 
     if (!isPathAbsolute) {
-      const matchingRoutes = matchRoutes(path, routes);
+      const matchingRoutes = matchRoutes(to, routes);
 
       matchingRoutes.forEach(({ component }) => {
         component.preload();

ui/src/views/Rules/ListRules/index.jsx
@@ -1570,12 +1570,7 @@ function ListRules(props) {
       </Drawer>
       <Snackbar onClose={handleSnackbarClose} {...snackbarState} />
       {!rewindDate && (
-        <Link
-          to={{
-            pathname: '/rules/create',
-            state: { rulesFilter: productChannelQueries },
-          }}
-        >
+        <Link to="/rules/create" state={{ rulesFilter: productChannelQueries }}>
           <Tooltip title="Add Rule">
             <Fab color="primary" className={classes.fab} disabled={rewindDate}>
               <AddIcon />