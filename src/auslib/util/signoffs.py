6b87bd690a104d4de48dbdb6871bfb6770ca4c32
Bastien Orivel <borivel@mozilla.com>
2025-12-04 16:12:04 +0100

    Refactor get_required_signoffs_for_product_channel to use list comprehensions
    This barely makes a difference in performance but I find this to be a
    hell of a lot more readable...

--- src/auslib/util/signoffs.py
+++ src/auslib/util/signoffs.py
@@ -16,13 +16,5 @@ def get_required_signoffs_for_product_channel(product, channel, product_rs_by_pr
     Channel supports globbing, so we must take that into account before deciding
     whether or not a signoff requirement is a match.
     """
-    rs_list = []
-    if product:
-        for rs in product_rs_by_product.get(product, []):
-            if not channel or matchRegex(channel, rs["channel"]):
-                rs_list.append(rs)
-    else:
-        for rs in all_product_rs:
-            if not channel or matchRegex(channel, rs["channel"]):
-                rs_list.append(rs)
-    return rs_list
+    source = product_rs_by_product.get(product, []) if product else all_product_rs
+    return [rs for rs in source if not channel or matchRegex(channel, rs["channel"])]
