efa3efad7c0d97b5a80af5a8fbb560f59261b4fa
Julien Cristau <jcristau@mozilla.com>
2026-06-25 19:29:30 +0200

    docs: fix invalid/deprecated sphinx config values
    language = None is no longer valid; source_suffix as a plain string
    is deprecated.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ templates_path = ["_templates"]
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = {".rst": "restructuredtext"}
 
 # The encoding of source files.
 #
@@ -77,7 +77,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
